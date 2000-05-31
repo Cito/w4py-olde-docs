f71ebc4263b5389273ff50b7b13dbd0f3a2cc568
Jay Love <jsliv@jslove.org>
2000-05-31 00:24:47 +0000

    Another DOS fix.  jeez.

--- PSP/__init__.py
+++ PSP/__init__.py
@@ -1,11 +1,11 @@
 # PSP
 # Webware for Python
 
-
+import os
 from PSPServletFactory import PSPServletFactory
 
 
 def InstallInWebKit(appServer):
 	app = appServer.application()
 	app.addServletFactory(PSPServletFactory(app))
-	app._Contexts['PSPExamples']='../PSP/Examples'
+	app._Contexts['PSPExamples']=os.path.normpath('../PSP/Examples')
