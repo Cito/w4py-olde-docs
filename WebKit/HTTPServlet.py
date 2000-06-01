289c6fcb8afa6ed592b9d4a3e2ccf1c4b4071ebd
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-06-01 03:28:32 +0000

    Replace bound method mapping with unbound method mapping to eliminate circular ref.

--- WebKit/HTTPServlet.py
+++ WebKit/HTTPServlet.py
@@ -7,50 +7,50 @@ class HTTPServlet(Servlet):
 	HTTPServlet implements the respond() method to invoke methods such as respondToGet() and respondToPut() depending on the type of HTTP request.
 	Current supported request are GET, POST, PUT, DELETE, OPTIONS and TRACE.
 	The dictionary _map contains the information about the types of requests and their corresponding methods.
-	
+
 	Note that HTTPServlet inherits awake() and respond() methods from Servlet and that subclasses may make use of these.
-	
+
 	See also: Servlet
 
 	FUTURE
 		* Document methods (take hints from Java HTTPServlet documentation)
 	'''
-	
+
 	## Init ##
-	
+
 	def __init__(self):
 		Servlet.__init__(self)
 		self._map = {
-			'GET':     self.respondToGet,
-			'POST':    self.respondToPost,
-			'PUT':     self.respondToPut,
-			'DELETE':  self.respondToDelete,
-			'OPTIONS': self.respondToOptions,
-			'TRACE':   self.respondToTrace
+			'GET':     self.__class__.respondToGet,
+			'POST':    self.__class__.respondToPost,
+			'PUT':     self.__class__.respondToPut,
+			'DELETE':  self.__class__.respondToDelete,
+			'OPTIONS': self.__class__.respondToOptions,
+			'TRACE':   self.__class__.respondToTrace
 		}
 
 
 	## Transactions ##
-	
+
 	def respond(self, trans):
 		''' Invokes the appropriate respondToSomething() method depending on the type of request (e.g., GET, POST, PUT, ...). '''
 		method = self._map[trans.request().method()]
-		method(trans)
-	
+		method(self, trans)
+
 	def respondToGet(self, trans):
 		raise SubclassResponsibilityError
-	
+
 	def respondToPost(self, trans):
 		raise SubclassResponsibilityError
 
 	def respondToPut(self, trans):
 		raise SubclassResponsibilityError
-	
+
 	def respondToDelete(self, trans):
 		raise SubclassResponsibilityError
-	
+
 	def respondToOptions(self, trans):
 		raise SubclassResponsibilityError
-	
+
 	def respondToTrace(self, trans):
 		raise SubclassResponsibilityError
