4dec36874e8cbbbf7b37251740338d970ac6b37a
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-06-02 22:46:35 +0000

    Added sentence in page about new 'admin' link.

--- WebKit/Examples/Welcome.py
+++ WebKit/Examples/Welcome.py
@@ -6,4 +6,5 @@ class Welcome(ExamplePage):
 		self.writeln('<p> Welcome to WebKit %s!' % self.application().server().version())
 		self.writeln('''<p> Along the top of this page you will find links to other example servlets/pages written with WebKit.
 <p> At the bottom of the page you will see a <b>source</b> link that will display for you the source code of the current example.
+<p> You will also see an <b>admin</b> link which will take you to the main administration page for the app server.
 ''')
