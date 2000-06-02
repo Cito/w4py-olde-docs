58fe554a0017498f00723aef74b4bf9b2e659c8b
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-06-02 22:52:36 +0000

    improved structure. added working dir info. added plug-ins info.

--- WebKit/_admin.py
+++ WebKit/_admin.py
@@ -3,6 +3,9 @@ from AdminPage import AdminPage
 from WebUtils.WebFuncs import URLEncode
 import os
 
+# @@ 2000-06-02 ce: We should show all the contexts of the application here
+
+
 class _admin(AdminPage):
 
 	def title(self):
@@ -11,22 +14,35 @@ class _admin(AdminPage):
 	def writeBody(self):
 		app = self.application()
 		curTime = time()
-		self.writeln('''
-			<table align=center cellspacing=0 cellpadding=0 borde=0>
-				<tr> <td><b>Version:</b></td> <td>%(ver)s</td> </tr>
-				<tr> <td><b>Global time:</b></td> <td>%(globalTime)s</td> </tr>
-				<tr> <td><b>Local time:</b></td> <td>%(localTime)s</td> </tr>
-				<tr> <td><b>Up since:</b></td> <td>%(upSince)s</td> </tr>
-				<tr> <td><b>Num requests:</b></td> <td>%(numRequests)d</td> </tr>
-			</table>
-		''' % {
-			'ver': app.webKitVersion(),
-			'globalTime': asctime(gmtime(curTime)),
-			'localTime': asctime(localtime(curTime)),
-			'upSince': asctime(localtime(app.server().startTime())),
-			'numRequests': app.server().numRequests()
-		})
+		self.writeGeneralInfo()
+		self.writePlugIns()
+		self.writeMenu()
+		self.writeSignature()
 
+	## General Info ##
+
+	def writeGeneralInfo(self):
+		app = self.application()
+		curTime = time()
+		info = [
+			('Version',      app.webKitVersion()),
+			('Global Time',  asctime(gmtime(curTime))),  # @@ 2000-06-02 ce: do we need global time?
+			('Local Time',   asctime(localtime(curTime))),
+			('Up Since',     asctime(localtime(app.server().startTime()))),
+			('Num Requests', app.server().numRequests()),
+			('Working Dir',  os.getcwd()),
+		]
+
+		self.writeln('<table align=center cellspacing=0 cellpadding=0 border=0>')
+		for label, value in info:
+			self.writeln('<tr> <td> <b>%s:</b> </td> <td>%s</td> </tr>' % (label, value))
+		self.writeln('</table>')
+
+
+	## Menu ##
+
+	def writeMenu(self):
+		app = self.application()
 		self.startMenu()
 		activityLogURL = '_dumpCSV?filename=%s' % URLEncode(app.setting('ActivityLogFilename'))
 		errorLogURL = '_dumpErrors?filename=%s' % URLEncode(app.setting('ErrorLogFilename'))
@@ -34,25 +50,6 @@ class _admin(AdminPage):
 		self.menuItem('Error log', errorLogURL, self.fileSize('ErrorLogFilename'))
 		self.menuItem('Show config', '_showConfig')
 		self.endMenu()
-
-		self.writeln('''
-<!--
-begin-parse
-{
-	'Version': %s,
-	'LocalTime': %s,
-	'GlobalTime': %s
-}
-end-parse
--->''' % (repr(app.webKitVersion()), repr(localtime(curTime)), repr(gmtime(curTime))))
-
-	def fileSize(self, filename):
-		''' Utility method for writeBody() to get the size of a configuration file. Returns an HTML string. '''
-		filename = self.application().setting(filename)
-		if os.path.exists(filename):
-			return '<font size=-1>(%0.0f KB)</font>' % (os.path.getsize(filename)/1024.0)
-		else:
-			return '<font size=-1>(does not exist)</font>'
 
 	def startMenu(self):
 		self.writeln('''<p><table align=center border=0 cellspacing=2 cellpadding=2 bgcolor=#FFFFDD>
@@ -67,3 +64,49 @@ end-parse
 
 	def endMenu(self):
 		self.writeln('</table>')
+
+	def fileSize(self, filename):
+		''' Utility method for writeMenu() to get the size of a configuration file. Returns an HTML string. '''
+		filename = self.application().setting(filename)
+		if os.path.exists(filename):
+			return '<font size=-1>(%0.0f KB)</font>' % (os.path.getsize(filename)/1024.0)
+		else:
+			return '<font size=-1>(does not exist)</font>'
+
+
+	## Plug-ins ##
+
+	def writePlugIns(self):
+		# @@ 2000-06-02 ce: We should allow a custom admin link for each plug-in (if it provides one)
+		# @@ 2000-06-02 ce: We should have a link to the plug-in's docs
+		plugIns = self.application().server().plugIns()
+		print '>> plug in', plugIns
+		if plugIns:
+			self.writeln('''<p><table align=center border=0 cellspacing=2 cellpadding=2>
+<tr bgcolor=black><td align=center colspan=3><font face="Arial, Helvetica" color=white><b>Plug-ins</b></font></td></tr>''')
+			for plugIn in plugIns:
+				name, dir, ver = plugIn.name(), plugIn.directory(), plugIn.version()
+				self.writeln('<tr> %(td)s %(name)s </td> %(td)s %(ver)s </td> %(td)s %(dir)s </td> </tr>' % {
+					'name': name,
+					'ver': ver,
+					'dir': dir,
+					'td': '<td bgcolor=#DDDDDD>'
+				})
+			self.writeln('</table>')
+
+
+	## Signature ##
+
+	def writeSignature(self):
+		app = self.application()
+		curTime = time()
+		self.writeln('''
+<!--
+begin-parse
+{
+	'Version': %s,
+	'LocalTime': %s,
+	'GlobalTime': %s
+}
+end-parse
+-->''' % (repr(app.webKitVersion()), repr(localtime(curTime)), repr(gmtime(curTime))))
