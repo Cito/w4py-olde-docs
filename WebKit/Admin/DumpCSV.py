e0aab669298f3161149e00548c9bcf07fb7403d5
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-06-26 18:08:48 +0000

    Moved admin scripts to Admin/ and made some tweaks.

---
+++ WebKit/Admin/DumpCSV.py
@@ -0,0 +1,89 @@
+import os, string
+from AdminPage import AdminPage
+
+
+def LoadCSV(filename):
+	''' Loads a CSV (comma-separated value) file from disk and returns it as a list of rows where each row is a list of values (which are always strings). '''
+
+	from MiscUtils import CSV
+	file = CSV.CSV()
+	file.load(filename, 0, 0)
+		# first  0 - first row is titles. I make this false so row[0] contains the headings
+		# second 0 - convert numbers. I don't need this.
+	return file
+
+	# 2000-05-03 ce: Old code that doesn't handle quotes
+	f = open(filename)
+	rows = []
+	numFields = None
+	lineNum = 1
+	while 1:
+		line = f.readline()
+		if not line:
+			break
+		rows.append(string.split(line, ','))
+		if numFields is None:
+			numFields = len(rows[0])
+		else:
+			assert len(rows[-1])<=numFields, 'Row %d has %d fields which exceeds the heading row which has %d.' % (lineNum, len(rows[-1]), numFields)
+		lineNum = lineNum + 1
+	f.close()
+	return rows
+
+
+class DumpCSV(AdminPage):
+
+	def filename(self):
+		''' Overidden by subclasses to specify what filename to show. '''
+		return None
+
+	def awake(self, trans):
+		AdminPage.awake(self, trans)
+		self._filename = self.filename()
+
+	def shortFilename(self):
+		return os.path.splitext(os.path.split(self._filename)[1])[0]
+
+	def title(self):
+		return 'View ' + self.shortFilename()
+
+	def writeBody(self):
+		if not os.path.exists(self._filename):
+			self.writeln('<p> File does not exist.')
+			return
+
+		rows = LoadCSV(self._filename)
+
+		if len(rows)==1:
+			plural = ''
+		else:
+			plural = 's'
+		self.writeln('<p>%d row%s' % (len(rows), plural))
+		self.writeln('<br><table align=center border=0 cellpadding=2 cellspacing=2>')
+
+
+		# Head row gets special formatting
+		self._headings = map(lambda name: string.strip(name), rows[0])
+		self._numCols = len(self._headings)
+		self.writeln('<tr bgcolor=black>')
+		for value in self._headings:
+			self.writeln('<td><font face="Arial, Helvetica" color=white><b> ', value, ' </b></font></td>')
+		self.writeln('</tr>')
+
+		# Data rows
+		rowIndex = 1
+		for row in rows[1:]:
+			self.writeln('<tr bgcolor=#EEEEEE>')
+			colIndex = 0
+			for value in row:
+				if colIndex<self._numCols: # for those cases where a row has more columns that the header row.
+					self.writeln('<td> ', self.cellContents(rowIndex, colIndex, value), ' </td>')
+				colIndex = colIndex + 1
+			self.writeln('</tr>')
+			rowIndex = rowIndex + 1
+
+		self.writeln('</table>')
+
+	def cellContents(self, rowIndex, colIndex, value):
+		''' This is a hook for subclasses to customize the contents of a cell based on any criteria (including location). '''
+		return value
---
+++ WebKit/Admin/Main.py
@@ -0,0 +1,114 @@
+from time import time, localtime, gmtime, asctime
+from AdminPage import AdminPage
+from WebUtils.WebFuncs import URLEncode
+import os
+
+# @@ 2000-06-02 ce: We should show all the contexts of the application here
+
+
+class Main(AdminPage):
+
+	def title(self):
+		return 'Admin'
+
+	def writeBody(self):
+		app = self.application()
+		curTime = time()
+		self.writeGeneralInfo()
+		self.writePlugIns()
+		self.writeMenu()
+		self.writeSignature()
+
+	## General Info ##
+
+	def writeGeneralInfo(self):
+		app = self.application()
+		curTime = time()
+		info = [
+			('Version',      app.webKitVersion()),
+			('Global Time',  asctime(gmtime(curTime))),  # @@ 2000-06-02 ce: do we need global time?
+			('Local Time',   asctime(localtime(curTime))),
+			('Up Since',     asctime(localtime(app.server().startTime()))),
+			('Num Requests', app.server().numRequests()),
+			('Working Dir',  os.getcwd()),
+		]
+
+		self.writeln('<table align=center cellspacing=0 cellpadding=0 border=0>')
+		for label, value in info:
+			self.writeln('<tr> <td> <b>%s:</b> </td> <td>%s</td> </tr>' % (label, value))
+		self.writeln('</table>')
+
+
+	## Menu ##
+
+	def writeMenu(self):
+		app = self.application()
+		self.startMenu()
+		#activityLogURL = '_dumpCSV?filename=%s' % URLEncode(app.setting('ActivityLogFilename'))
+		activityLogURL = 'Access'
+#		errorLogURL = '_dumpErrors?filename=%s' % URLEncode(app.setting('ErrorLogFilename'))
+		errorLogURL = 'Errors'
+		self.menuItem('Activity log', activityLogURL, self.fileSize('ActivityLogFilename'))
+		self.menuItem('Error log', errorLogURL, self.fileSize('ErrorLogFilename'))
+		self.menuItem('Show config', 'Config')
+		self.endMenu()
+
+	def startMenu(self):
+		self.writeln('''<p><table align=center border=0 cellspacing=2 cellpadding=2 bgcolor=#FFFFDD>
+<tr bgcolor=black><td align=center><font face="Arial, Helvetica" color=white><b>Menu</b></font></td></tr>''')
+
+	def menuItem(self, title, url, extra=None):
+		if extra:
+			extra = extra + ' '
+		else:
+			extra = ''
+		self.writeln('<tr><td> <a href="%s">%s</a> %s</td></tr>' % (url, title, extra))
+
+	def endMenu(self):
+		self.writeln('</table>')
+
+	def fileSize(self, filename):
+		''' Utility method for writeMenu() to get the size of a configuration file. Returns an HTML string. '''
+		filename = self.application().setting(filename)
+		if os.path.exists(filename):
+			return '<font size=-1>(%0.0f KB)</font>' % (os.path.getsize(filename)/1024.0)
+		else:
+			return '<font size=-1>(does not exist)</font>'
+
+
+	## Plug-ins ##
+
+	def writePlugIns(self):
+		# @@ 2000-06-02 ce: We should allow a custom admin link for each plug-in (if it provides one)
+		# @@ 2000-06-02 ce: We should have a link to the plug-in's docs
+		plugIns = self.application().server().plugIns()
+		print '>> plug in', plugIns
+		if plugIns:
+			self.writeln('''<p><table align=center border=0 cellspacing=2 cellpadding=2>
+<tr bgcolor=black><td align=center colspan=3><font face="Arial, Helvetica" color=white><b>Plug-ins</b></font></td></tr>''')
+			for plugIn in plugIns:
+				name, dir, ver = plugIn.name(), plugIn.directory(), plugIn.version()
+				self.writeln('<tr> %(td)s %(name)s </td> %(td)s %(ver)s </td> %(td)s %(dir)s </td> </tr>' % {
+					'name': name,
+					'ver': ver,
+					'dir': dir,
+					'td': '<td bgcolor=#DDDDDD>'
+				})
+			self.writeln('</table>')
+
+
+	## Signature ##
+
+	def writeSignature(self):
+		app = self.application()
+		curTime = time()
+		self.writeln('''
+<!--
+begin-parse
+{
+	'Version': %s,
+	'LocalTime': %s,
+	'GlobalTime': %s
+}
+end-parse
+-->''' % (repr(app.webKitVersion()), repr(localtime(curTime)), repr(gmtime(curTime))))
