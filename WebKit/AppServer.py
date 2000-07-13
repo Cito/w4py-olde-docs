240bfdb98d8d89116292c253d5a4b20a79c450df
Jay Love <jsliv@jslove.org>
2000-07-13 00:56:14 +0000

    Add AsyncThreadedAppServer, Threaded is still default. Cleanup Can startup code in Application. Fix Sweeper thread hang in Application.

--- WebKit/AppServer.py
+++ WebKit/AppServer.py
@@ -31,33 +31,34 @@ class AppServerError(Exception):
 
 
 class AppServer(Configurable):
-	"""
-	"""
+    """
+    """
 
 	## Init ##
 
-	def __init__(self):
-		self._startTime = time.time()
-		Configurable.__init__(self)
-		self._verbose = self.setting('Verbose')
-		self._plugIns = []
-		self._reqCount = 0
+    def __init__(self):
+        self._startTime = time.time()
+        Configurable.__init__(self)
+        self._verbose = self.setting('Verbose')
+        self._plugIns = []
+        self._reqCount = 0
+        self._addr = None
 
-		self.config() # cache the config
-		self.printStartUpMessage()
-		self._app = self.createApplication()
-		self.loadPlugIns()
+        self.config() # cache the config
+        self.printStartUpMessage()
+        self._app = self.createApplication()
+        self.loadPlugIns()
 
-		self.running = 1
+        self.running = 1
 
 
-	def shutDown(self):
-		'''
+    def shutDown(self):
+		"""
 		Subclasses may override and normally follow this sequence:
 			1. set self._shuttingDown to 1
 			2. class specific statements for shutting down
 			3. Invoke super's shutDown() e.g., AppServer.shutDown(self)
-		'''
+		"""
 
 		self._shuttingDown = 1
 		self.running = 0
@@ -73,95 +74,96 @@ class AppServer(Configurable):
 		del self._app
 
 
-	## Configuration ##
+    ## Configuration ##
 
-	def defaultConfig(self):
-		return DefaultConfig
+    def defaultConfig(self):
+        return DefaultConfig
 
-	def configFilename(self):
-		return 'Configs/AppServer.config'
-
-
-	## Network Server ##
-
-	def createApplication(self):
-		''' Creates and returns an application object. Invoked by __init__. '''
-		return Application(server=self)
-
-	def printStartUpMessage(self):
-		''' Invoked by __init__. '''
-		print 'WebKit AppServer', self.version()
-		print 'part of Webware for Python'
-		print 'Copyright 1999-2000 by Chuck Esterbrook. All Rights Reserved.'
-		print 'WebKit and Webware are open source.'
-		print 'Please visit:  http://webware.sourceforge.net'
-		print
-		print 'Process id is', os.getpid()
-		print
-		if self.setting('PrintConfigAtStartUp'):
-			self.printConfig()
+    def configFilename(self):
+        return 'Configs/AppServer.config'
 
 
-	## Plug-ins ##
+    ## Network Server ##
 
-	def plugIns(self):
-		''' Returns a list of the plug-ins loaded by the app server. Each plug-in is a python package. '''
-		return self._plugIns
+    def createApplication(self):
+        ''' Creates and returns an application object. Invoked by __init__. '''
+        return Application(server=self)
 
-	def loadPlugIn(self, path):
-		''' Loads the given plug-in. Used by loadPlugIns(). '''
-		try:
-			plugIn = PlugIn(self, path)
-			self._plugIns.append(plugIn)
-			plugIn.load()
-			plugIn.install()
-		except:
-			import traceback
-			traceback.print_exc(file=sys.stderr)
-			self.error('Plug-in %s raised exception.' % path)
-
-	def loadPlugIns(self):
-		"""
-		A plug-in allows you to extend the functionality of WebKit without necessarily having to modify it's source. Plug-ins are loaded by AppServer at startup time, just before listening for requests. See the docs for PlugIn.py for more info.
-		"""
-		# @@ 2000-05-21 ce: We should really have an AddPlugIns and RemovePlugIns setting
-		for plugInPath in self.setting('PlugIns'):
-			self.loadPlugIn(plugInPath)
+    def printStartUpMessage(self):
+        ''' Invoked by __init__. '''
+        print 'WebKit AppServer', self.version()
+        print 'part of Webware for Python'
+        print 'Copyright 1999-2000 by Chuck Esterbrook. All Rights Reserved.'
+        print 'WebKit and Webware are open source.'
+        print 'Please visit:  http://webware.sourceforge.net'
+        print
+        print 'Process id is', os.getpid()
+        print
+        if self.setting('PrintConfigAtStartUp'):
+            self.printConfig()
 
 
-	## Misc ##
+    ## Plug-ins ##
 
-	def version(self):
-		# @@ 2000-07-10 ce: Resolve this with the fooVersion() methods in Application
-		return '0.4 PRERELEASE'
+    def plugIns(self):
+        ''' Returns a list of the plug-ins loaded by the app server. Each plug-in is a python package. '''
+        return self._plugIns
 
-	def application(self):
-		return self._app
+    def loadPlugIn(self, path):
+        ''' Loads the given plug-in. Used by loadPlugIns(). '''
+        try:
+            plugIn = PlugIn(self, path)
+            self._plugIns.append(plugIn)
+            plugIn.load()
+            plugIn.install()
+        except:
+            import traceback
+            traceback.print_exc(file=sys.stderr)
+            self.error('Plug-in %s raised exception.' % path)
 
-	def startTime(self):
-		''' Returns the time the app server was started (as seconds, like time()). '''
-		return self._startTime
+    def loadPlugIns(self):
+        """
+        A plug-in allows you to extend the functionality of WebKit without necessarily having to modify it's source. Plug-ins are loaded by AppServer at startup time, just before listening for requests. See the docs for PlugIn.py for more info.
+        """
+        # @@ 2000-05-21 ce: We should really have an AddPlugIns and RemovePlugIns setting
+        for plugInPath in self.setting('PlugIns'):
+            self.loadPlugIn(plugInPath)
 
-	def numRequests(self):
-		''' Return the number of requests received by this server since it was launched. '''
-		return self._reqCount
+
+    ## Misc ##
+
+    def version(self):
+        # @@ 2000-07-10 ce: Resolve this with the fooVersion() methods in Application
+        return '0.4 PRERELEASE'
+
+    def application(self):
+        return self._app
+
+    def startTime(self):
+        ''' Returns the time the app server was started (as seconds, like time()). '''
+        return self._startTime
+
+    def numRequests(self):
+        ''' Return the number of requests received by this server since it was launched. '''
+        return self._reqCount
 
 
-	## Warnings and Errors ##
+    ## Warnings and Errors ##
 
-	def warning(self, msg):
-		# @@ 2000-04-25 ce: would this be useful?
-		raise NotImplementedError
+    def warning(self, msg):
+        # @@ 2000-04-25 ce: would this be useful?
+        raise NotImplementedError
 
-	def error(self, msg):
-		"""
-		Flushes stdout and stderr, prints the message to stderr and exits with code 1.
-		"""
-		sys.stdout.flush()
-		sys.stderr.flush()
-		sys.stderr.write('ERROR: %s\n' % msg)
-		sys.stderr.flush()
-		sys.exit(1)  # @@ 2000-05-29 ce: Doesn't work. Perhaps because of threads
+    def error(self, msg):
+        """
+        Flushes stdout and stderr, prints the message to stderr and exits with code 1.
+        """
+        sys.stdout.flush()
+        sys.stderr.flush()
+        sys.stderr.write('ERROR: %s\n' % msg)
+        sys.stderr.flush()
+        sys.exit(1)  # @@ 2000-05-29 ce: Doesn't work. Perhaps because of threads
+
 
 
 def main():
---
+++ WebKit/AsyncThreadedAppServer.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+"""
+Version of AppServer that uses threads and the asyncore module.
+"""
+
+
+from AppServer import AppServer
+from Application import Application
+from marshal import dumps, loads
+import os, sys
+from threading import Lock, Thread
+import Queue
+import select
+import socket
+import asyncore
+
+
+
+
+class AsyncThreadedAppServer(asyncore.dispatcher,AppServer):
+    """
+
+    """
+
+    ## Init ##
+
+    def __init__(self):
+        
+        AppServer.__init__(self)
+
+        self._poolsize=self.setting('ServerThreads')
+
+        self.threadPool=[]
+        self.requestQueue=Queue.Queue(self._poolsize*5) #5 times the number of threads we have
+        self.rhQueue=Queue.Queue(0)#must be larger than requestQueue, just make it no limit, and limit the number that can be created
+        self._maxRHCount = self._poolsize * 10
+        self.rhCreateCount = 0
+
+        print "Port",self.setting("Port")
+        self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.bind(('localhost',self.setting('Port')))
+        print "Listening On",self.address()
+
+        addr=self.address()
+        open('address.text', 'w').write('%s:%d' % (addr[0], addr[1]))
+
+
+        print 'OK'
+        print
+        self.running=1
+
+        print "spawning threads "
+        for i in range(self._poolsize): #change to threadcount
+            t=Thread(target=self.threadloop)
+            t.start()
+            self.threadPool.append(t)
+            sys.stdout.write(".")
+        sys.stdout.write("\n")
+
+
+        #self.asyn_thread = Thread(target=self.asynloop)
+        #self.asyn_thread.start()
+
+        self.listen(1024)
+        print "Ready"
+
+
+    def handle_accept(self):
+        """
+        This is the function that starts the request processing cycle.  When asyncore senses a write on the main socket, it calls this function.  We then accept the connection, and hand it off to a RequestHandler instance by calling the RequestHandler instance's activate method.  When we call that activate method, the RH registers itself with asyncore by calling set_socket, so that asyncore now will include that socket, and thus the RH, in it's network select loop.
+        """
+        #print "Accepting"
+        conn,addr = self.accept()
+        conn.setblocking(0)
+        self._reqCount = self._reqCount+1
+        rh = None
+        try:
+            rh=self.rhQueue.get_nowait()
+        except Queue.Empty:
+            if self.rhCreateCount < self._maxRHCount:
+                rh = RequestHandler(self)
+                self.rhCreateCount = self.rhCreateCount + 1
+            else:
+                rh = self.rhQueue.get() #block
+        rh.activate(conn)
+
+    def readable(self):
+        return self.accepting
+
+    def writable(self):
+        return 0
+
+    def handle_connect(self):
+        pass
+
+    def log(self, message):
+       pass
+
+    def asynloop(self):
+        """
+        Not used currently
+        """
+        while self.running:
+            #print "asyncore loop"
+            asyncore.loop()
+
+    def address(self):
+                if self._addr is None:
+                        self._addr = ('', self.setting('Port'))
+                return self._addr
+
+
+
+
+    def threadloop(self):
+        """
+        Try to get a RequestHandler instance off the requestQueue and process it.
+        """
+        while self.running:
+            try:
+                rh=self.requestQueue.get()
+                if rh == None: #None means time to quit
+                    break                                                               
+                rh.handleRequest()      #this is all there is to it             
+            except Queue.Empty:
+                pass
+
+
+    def shutDown(self):
+        """
+        Cleanup when being shut down.
+        """
+        self._app.shutDown()
+        #self.mainsocket.close()
+        for i in range(self._poolsize):
+            self.requestQueue.put(None)#kill all threads
+        for i in self.threadPool:
+            i.join()
+        del self._plugIns[:]
+
+        del self._app
+
+
+
+import string, time
+
+class RequestHandler(asyncore.dispatcher):
+    """
+    Has the methods that process the request.
+    An instance of this class is activated by AppServer.  When activated, it is listening for the request to come in.  asyncore will call handle_read when there is data to be read.  ONce all the request has been read, it will put itself in the requestQueue to be picked up by a thread and processed by calling handleRequest. Once the processing is complete, the thread drops the instance and tries to find another one.  This instance notifies asyncore that it is ready to send.
+    """
+
+
+    def __init__(self, server):
+        self.server=server
+        self.have_request = 0
+        self.have_response = 0
+        self.reqdata=[]
+        self._buffer = ''
+
+    def handleRequest(self):
+
+        verbose = self.server._verbose
+
+        startTime = time.time()
+        if verbose:
+            print 'BEGIN REQUEST'
+            print time.asctime(time.localtime(startTime))
+
+        if verbose:
+            print 'receiving request from', self.socket
+
+        while not self.have_request:
+            print ">> Should always have request before we get here"
+            time.sleep(0.01)
+
+        if verbose:
+            print 'received %d bytes' % len(self.reqdata)
+        if self.reqdata:
+            dict = loads(self.reqdata)
+            if verbose:
+                print 'request has keys:', string.join(dict.keys(), ', ')
+
+
+        transaction = self.server._app.dispatchRawRequest(dict)
+        rawResponse = dumps(transaction.response().rawResponse())
+
+        self._buffer = rawResponse
+        self.have_response = 1
+
+        if verbose:
+            print 'connection closed.'
+            print '%0.2f secs' % (time.time() - startTime)
+            print 'END REQUEST'
+            print
+
+        transaction._application=None
+        transaction.die()
+        del transaction
+
+    def activate(self, socket):
+        self.set_socket(socket)
+        self._buffer=''
+        self.active = 1
+
+    def readable(self):
+        return self.active and not self.have_request
+
+    def writable(self):
+        """
+        Always ready to write, otherwise we might have to wait for a timeout to be asked again
+        """
+        return self.active
+        #return self.have_response
+
+    def handle_connect(self):
+        pass
+
+    def handle_read(self):
+        data = self.recv(8192)
+        if len(data) == 8192:
+            self.reqdata.append(data)
+        else:
+            self.reqdata.append(data)
+            self.reqdata = string.join(self.reqdata,'')
+            self.have_request=1
+            self.server.requestQueue.put(self)
+            #self.socket.shutdown(0)
+
+    def handle_write(self):
+        if not self.have_response: return
+        sent = self.send(self._buffer)
+        self._buffer = self._buffer[sent:]
+        if len(self._buffer) == 0:
+            self.close()
+            #For testing
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+    def close(self):
+        self.active = 0
+        self.have_request = 0
+        self.have_response = 0
+        self.reqdata=[]
+        asyncore.dispatcher.close(self)
+        self.server.rhQueue.put(self)
+
+
+    def log(self, message):
+        pass
+
+
+
+
+
+def main():
+    try:
+        server = AsyncThreadedAppServer()
+        asyncore.loop()
+    except Exception, e: #Need to kill the Sweeper thread somehow
+        print e
+        print "Exiting AppServer"
+        server.running=0
+        server.shutDown()
+        del server
+        sys.exit()
+
+
+if __name__=='__main__':
+    main()
