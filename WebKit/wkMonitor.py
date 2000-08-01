a47dfc7a4f19b3fd608827214aa9c14f803ada2f
Jay Love <jsliv@jslove.org>
2000-08-01 02:39:47 +0000

    Add failover monitor.  INitial commit.

---
+++ WebKit/wkMonitor.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+
+"""
+Quick and dirty fault tolerance system for WebKit
+
+author: Jay Love
+
+(Or maybe I don't want to put my name on this one? ;0 )
+
+
+Future:
+Add ability to limit number of requests served.  When some count is reached, send
+a message to the server to save it's sessions, then exit.  Then start a new AppServer
+that will pick up those sessions.
+
+It should be possible on both Unix and Windows to monitor the AppServer process in 2 ways:
+1) The method used here, ie can it service requests?
+2) is the process still running?
+
+Item 2) lends itself to load balancing of some kind.
+
+
+"""
+
+import AppServer
+import socket
+import time
+import AsyncThreadedAppServer
+import os
+import asyncore
+import sys
+import signal
+import string
+
+
+#Windows Specific Section
+if os.name == 'nt':
+	import win32process
+#These need to be filled in correctly on Windows
+pythonexec = "C:\progra~1\python\python"
+appserver = "e:\Linux\python\webware\webkit\AsyncThreadedAppServer.py"
+
+
+
+srvpid = 0
+interval = 10  #add to config if this implementation is adopted
+maxStartTime = 120
+
+cfg = open("Configs/AppServer.config")
+cfg = eval(cfg.read())
+addr = ((cfg['Host'],cfg['Port']))
+
+debug = 1
+
+
+def createServer():
+	"""Unix only, after forking"""
+	print "Starting Server"
+	AsyncThreadedAppServer.main()
+
+
+def startupCheck():
+	"""
+	Make sure the AppServer starts up correctly.
+	"""
+	count = 0
+	while 1: #give the server a chance to start
+		if checkServer(0):
+			break
+		print "Waiting for start"
+		time.sleep(interval)
+		count = count + interval
+		if count > maxStartTime:
+			print "Couldn't start AppServer"
+			if os.name == 'posix':
+				print "Killing AppServer"
+				os.kill(srvpid,signal.SIGKILL)
+			else:
+				win32process.TerminateProcess(srvpid,0)
+			sys.exit(1)
+
+
+def startServer(killcurrent = 1):
+	"""
+	Start the AppServer.
+	"""
+	global srvpid
+	if os.name == 'posix':
+		if killcurrent:
+			os.kill(srvpid,signal.SIGQUIT)
+			os.waitpid(srvpid,0) #prevent zombies
+		srvpid = os.fork()
+		if srvpid == 0:
+			createServer()
+			sys.exit()
+	elif os.name == 'nt':
+		if killcurrent:
+			win32process.TerminateProcess(srvpid,0)
+		print "Starting %s" % appserver
+		srvpid = os.spawnve(os.P_NOWAIT, pythonexec, [pythonexec,appserver],os.environ)
+	else:
+		print "I don't recognize your OS!\nSorry, but I must quit.\n You can run the AppServer b y itself, though!"
+		sys.exit(0)
+	
+	
+
+def checkServer(restart = 1):
+	"""
+	Send a check request to the AppServer.  If restart is 1, then
+	attempt to restart the server if we can't connect to it.
+	This function needs more thought, b/c this will kill a server that is
+	running but is VERY busy.
+	"""
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(addr)
+		s.send("STATUS")
+		s.shutdown(1)
+		resp = s.recv(9)#up to 1 billion requests!
+		if debug:
+			print "Processed %s Requests" % resp
+		return 1
+	except:
+		if restart:
+			startServer()
+			startupCheck()
+		else:
+			return 0
+		
+
+
+def main():
+	startServer(0)
+	startupCheck()
+	
+	while 1:
+		try: 
+			time.sleep(interval)
+			checkServer()
+		except Exception , e:
+			print e
+			if os.name == 'posix':
+				print "Killing AppServer"
+				os.kill(srvpid,signal.SIGQUIT)
+			else:
+				win32process.TerminateProcess(srvpid,0)
+			sys.exit()
+		
+
+
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
