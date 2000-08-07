35848d9710103bf109996015fbd30cc0edfe51c1
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-08-07 21:13:51 +0000

    Make subclass of Adapter. Added configuration. Added word wrapping for captured output.

--- WebKit/OneShotAdapter.py
+++ WebKit/OneShotAdapter.py
@@ -10,60 +10,105 @@ An example, URL:
 '''
 
 
+# 2000-08-07 ce: For accuracy purposes, we want to record the timestamp as early as possible.
 import time
-timestamp = time.time()
+_timestamp = time.time()
 
+
+# 2000-08-07 ce: We have to reassign sys.stdout *immediately* because it's referred to as a default parameter value in Configurable.py which happens to be our ancestor class as well as the ancestor class of AppServer and Application. The Configurable method that uses sys.stdout for a default parameter value must not execute before we rewire sys.stdout. Tricky, tricky.
 try:
 	from cStringIO import StringIO
 except ImportError:
 	from StringIO import StringIO
-
-
-# @@ 2000-07-10 ce fix this up
 import sys
-sys.path.append('..')
-import WebUtils
-from WebUtils.WebFuncs import HTMLEncode
+_real_stdout = sys.stdout
+sys.stdout = _console = StringIO()  # to capture the console output of the application
+
+
+
+import os, string
+
+from Adapter import *
+
+
+class OneShotAdapter(Adapter):
+
+	def defaultConfig(self):
+		config = Adapter.defaultConfig(self)
+		config.update({
+			'ShowConsole':           0,
+			'ConsoleWidth':          80,  # use 0 to turn off
+			'ConsoleHangingIndent':  4,
+		})
+		return config
+
+	def run(self):
+
+		try:
+			myInput = sys.stdin.read()
+
+			dict = {
+				'format':  'CGI',
+				'time':    _timestamp,
+				'environ': os.environ.data, # ce: a little tricky. We use marshal which only works on built-in types, so we need environ's dictionary
+				'input':   myInput
+			}
+
+			print 'ONE SHOT MODE\n'
+
+			from OneShotAppServer import OneShotAppServer
+			appSvr = OneShotAppServer()
+			response = appSvr.dispatchRawRequest(dict).response().rawResponse()
+			appSvr.shutDown()
+			appSvr = None
+
+			sys.stdout = _real_stdout
+			if os.name=='nt': # MS Windows: no special translation of end-of-lines
+				import msvcrt
+				msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+			write = sys.stdout.write
+			for pair in response['headers']:
+				write('%s: %s\n' % pair)
+			write('\n')
+			write(response['contents'])
+			if self.setting('ShowConsole'):
+				self.showConsole(_console.getvalue())
+		except:
+			import traceback
+
+			sys.stderr.write('[%s] [error] WebKitCGIAdaptor: Error while responding to request (unknown)\n' % (time.asctime(time.localtime(time.time()))))
+			sys.stderr.write('Python exception:\n')
+			traceback.print_exc(file=sys.stderr)
+
+			output = apply(traceback.format_exception, sys.exc_info())
+			output = string.join(output, '')
+			output = string.replace(output, '&', '&amp;')
+			output = string.replace(output, '<', '&lt;')
+			output = string.replace(output, '>', '&gt;')
+			output = string.replace(output, '"', '&quot;')
+			sys.stdout.write('''Content-type: text/html
+
+<html><body>
+<p><pre>ERROR
+
+%s</pre>
+</body></html>\n''' % output)
+
+	def showConsole(self, contents):
+		width = self.setting('ConsoleWidth')
+		if width:
+			contents = WordWrap(contents, self.setting('ConsoleWidth'), self.setting('ConsoleHangingIndent'))
+		contents = HTMLEncode(contents)
+		sys.stdout.write('<br><p><table><tr><td bgcolor=#EEEEEE><pre>%s</pre></td></tr></table>' % contents)
 
 
 def main():
-	import os, string, sys
-
 	try:
-		myInput = sys.stdin.read()
-
-		dict = {
-			'format':  'CGI',
-			'time':    timestamp,
-			'environ': os.environ.data, # ce: a little tricky. We use marshal which only works on built-in types, so we need environ's dictionary
-			'input':   myInput
-		}
-
-		real_stdout = sys.stdout
-		sys.stdout = console = StringIO()  # to capture the console output of the application
-		print 'ONE SHOT MODE\n'
-
-		from OneShotAppServer import OneShotAppServer
-		appSvr = OneShotAppServer()
-		response = appSvr.dispatchRawRequest(dict).response().rawResponse()
-		appSvr.shutDown()
-		appSvr = None
-
-		sys.stdout = real_stdout
-		if os.name=='nt': # MS Windows: no special translation of end-of-lines
-			import msvcrt
-			msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
-		write = sys.stdout.write
-		for pair in response['headers']:
-			write('%s: %s\n' % pair)
-		write('\n')
-		write(response['contents'])
-		write('<br><p><table><tr><td bgcolor=#EEEEEE><pre>%s</pre></td></tr></table>' % HTMLEncode(console.getvalue()))
+		OneShotAdapter().run()
 	except:
 		import traceback
 
-		sys.stderr.write('[%s] [error] WebKitCGIAdaptor: Error while responding to request (unknown)\n' % (
-			time.asctime(time.localtime(time.time()))))
+		sys.stderr.write('[%s] [error] OneShotAdapter: Error while responding to request (unknown)\n' % (time.asctime(time.localtime(time.time()))))
 		sys.stderr.write('Python exception:\n')
 		traceback.print_exc(file=sys.stderr)
 
