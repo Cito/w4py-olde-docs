2de73fe6d2cc826424bf6ab43ce1b77235e2ace1
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-08-11 22:18:29 +0000

    Updated explanation text.

--- WebKit/Examples/Welcome.py
+++ WebKit/Examples/Welcome.py
@@ -3,7 +3,7 @@ from ExamplePage import ExamplePage
 class Welcome(ExamplePage):
 
 	def writeContent(self):
-		self.writeln('<p> Welcome to WebKit%s!' % self.application().webKitVersion())
+		self.writeln('<p> Welcome to WebKit %s!' % self.application().webKitVersion())
 		self.writeln('''<p> Along the side of this page you will see various links that will take you to:
 
 		<ul>
@@ -11,6 +11,8 @@ class Welcome(ExamplePage):
 			<li> The source code of the current example.
 			<li> The local WebKit documentation.
 			<li> Whatever contexts have been configured. Each context represents a distinct set of web pages, usually given a descriptive name.
-			<li> The <b>Admin</b> context is particularly interesting because it takes you to the administrative pages for the WebKit AppServer where you can review logs, configuration, plug-ins, etc.
 			<li> External sites, such as the Webware home page.
+		</ul>
+
+		<p> The <b>Admin</b> context is particularly interesting because it takes you to the administrative pages for the WebKit AppServer where you can review logs, configuration, plug-ins, etc.
 ''')
