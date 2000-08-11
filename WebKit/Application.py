714cbc3e56343036d1784e0c5169efee2dd26647
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-08-11 22:43:50 +0000

    Moved some NamedValueAccess related code to MiscUtils.__init__.py

--- WebKit/Application.py
+++ WebKit/Application.py
@@ -22,24 +22,6 @@ from WebUtils.HTMLForException import HTMLForException
 # @@ from MiscUtils.NamedValueAccess import NamedValueAccess
 import MiscUtils.CSV
 
-# Beef up UserDict with the NamedValueAccess base class and custom versions of
-# hasValueForKey() and valueForKey(). This all means that UserDict's (such as
-# os.environ) are key/value accessible. At some point, this probably needs to
-# move somewhere else as other Webware components will need this "patch".
-# (@@ 2000-05-07 ce: Plus I just duplicated this from CGIWrapper.py. Doh!)
-import MiscUtils
-from MiscUtils.NamedValueAccess import NamedValueAccess
-if not NamedValueAccess in UserDict.__bases__:
-	UserDict.__bases__ = UserDict.__bases__ + (NamedValueAccess,)
-
-	def _UserDict_hasValueForKey(self, key):
-		return self.has_key(key)
-
-	def _UserDict_valueForKey(self, key, default=None): # @@ 2000-05-10 ce: does Tombstone fit here and possibly in NamedValueAccess?
-		return self.get(key, default)
-
-	setattr(UserDict, 'hasValueForKey', _UserDict_hasValueForKey)
-	setattr(UserDict, 'valueForKey', _UserDict_valueForKey)
 
 
 class ApplicationError(Exception):
