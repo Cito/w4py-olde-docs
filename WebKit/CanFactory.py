01a3805277af4c038f1d39625f68034af05df8b0
Jay Love <jsliv@jslove.org>
2000-08-12 13:30:36 +0000

    add multiple can directories.

--- WebKit/CanFactory.py
+++ WebKit/CanFactory.py
@@ -1,15 +1,16 @@
 
 import os
+import imp
 
 class CanFactory:
-    """Creates Cans on demand.  Looks only in the Cans directory of WebKit."""
-    def __init__(self, app,canDir):
-	    self.__CanClasses={}
-	    self.__App = app
+	"""Creates Cans on demand.  Looks only in the Cans directorys."""
+	def __init__(self, app):
+		self._canClasses={}
+		self._app = app
 	    #self.__CanDir = app.getCanDir()
-	    self.__CanDir=canDir
+		self._canDirs=app._canDirs
 
-    def createCan(self,canName,*args,**kargs):
+	def _createCan(self,canName,*args,**kargs):
 		if self.__CanClasses.has_key(canName):
 			klass=self.__CanClasses[canName]
 		else:
@@ -30,6 +31,23 @@ class CanFactory:
 		else: instance = apply(klass,args,kargs)
 
 		return instance
-	
-	
+
+	def createCan(self, canName, *args, **kwargs):
+		if self._canClasses.has_key(canName):
+			return self._canClasses[canName]()
+		
+		self.canDirs = self._app._canDirs
+		res = imp.find_module(canName,self._canDirs)
+		mod = imp.load_module(canName, res[0], res[1], res[2])
+		klass = mod.__dict__[canName]
+		self._canClasses[canName]=klass
+		
+		if len(args)==0 and len(kwargs)==0:
+			instance = klass()
+		elif len(args)==0:
+			instance = apply(klass,kwargs)
+		elif len(kwargs)==0:
+			instance = apply(klass,args)
+		else: instance = apply(klass,args,kwargs)
+		return instance
 	
