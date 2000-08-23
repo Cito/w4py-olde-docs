3ec1fec76b83de1dd010ca4a09155fc9d53e5429
Jay Love <jsliv@jslove.org>
2000-08-23 01:53:34 +0000

    This little prog will stop a running AppServer started with wkMonitor.py.

---
+++ WebKit/stopMonitor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import socket
+import time
+import os
+import sys
+import signal
+import string
+
+
+#Windows Specific Section
+if os.name == 'nt':
+	import win32process
+
+
+pid = int(open("monitorpid.txt","r").read())
+
+
+if os.name == "posix":
+	os.kill(pid,signal.SIGINT)
+else:
+	win32process.TerminateProcess(srvpid,0)
+
+
