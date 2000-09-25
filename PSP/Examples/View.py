6c2c5fe5929fd1082b7b27a53dc5698790907a2e
Jay Love <jsliv@jslove.org>
2000-09-25 22:47:19 +0000

    Add ability to view PSP source.

---
+++ PSP/Examples/View.py
@@ -0,0 +1,28 @@
+import string
+from Page import Page
+import os
+
+class View(Page):
+	"""
+
+	"""
+
+	def writeBody(self):
+		req = self.request()
+		if req.hasField('filename'):
+			self.writeln('<p>', self.__class__.__doc__)
+
+			filename = req.field('filename')
+
+			if not os.path.exists(filename):
+				self.write("No such file %s exists" % filename)
+				return
+
+			text = open(filename).read()
+
+			text = string.replace(text,"<","&lt;")
+			text = string.replace(text,">","&gt;")
+			text = string.replace(text,'\n',"<br>")
+
+			self.write(text)
+			
