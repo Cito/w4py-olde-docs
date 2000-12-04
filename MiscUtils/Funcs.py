e6ac3b4cc932d85ce8ca606f97232e8c6614ac12
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-12-04 22:21:23 +0000

    Added CharWrap().

--- MiscUtils/Funcs.py
+++ MiscUtils/Funcs.py
@@ -22,3 +22,30 @@ def Commas(number):
 			break
 		number[i:i] = [',']
 	return ''.join(number)
+
+
+def CharWrap(s, width, hanging=0):
+	''' Returns a new version of the string word wrapped with the given width and hanging indent. The font is assumed to be monospaced.
+	This can be useful for including text between <pre> </pre> tags since <pre> will not word wrap and for lengthly lines, will increase the width of a web page.
+	It can also be used to help delineate the entries in log-style output by passing hanging=4.
+	'''
+	import string
+	if not s:
+		return s
+	assert hanging<width
+	hanging = ' ' * hanging
+	lines = string.split(s, '\n')
+	i = 0
+	while i<len(lines):
+		s = lines[i]
+		while len(s)>width:
+			t = s[width:]
+			s = s[:width]
+			lines[i] = s
+			i = i + 1
+			lines.insert(i, None)
+			s = hanging + t
+		else:
+			lines[i] = s
+		i = i + 1
+	return string.join(lines, '\n')
