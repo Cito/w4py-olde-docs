4a1d562b4ea6224830323de79968c94c878c4b4b
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2000-12-24 00:14:35 +0000

    Added Properties.py and doc dir.

---
+++ COMKit/Properties.py
@@ -0,0 +1,13 @@
+name = 'COMKit'
+
+version = (0, 1, 0, 'pre')
+
+docs = [
+	#{'name': "User's Guide", 'file': 'UsersGuide.html'},
+]
+
+status = 'alpha'
+
+requiredPyVersion = (1, 5, 2)
+
+synopsis = '''COMKit allows COM objects such as ADO to be used in free-threading mode in a threaded app server.  See Appendix D of the fine book Python Programming on Win32 by Mark Hammond and Andy Robinson for details.'''
