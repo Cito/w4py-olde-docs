1b2685bc38c85e198d5e737e10a940090f1d721c
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-01-08 00:19:36 +0000

    Fixed synopsis.

--- MiscUtils/Properties.py
+++ MiscUtils/Properties.py
@@ -10,4 +10,4 @@ status = 'beta'
 
 requiredPyVersion = (1, 5, 2)
 
-synopsis = '''For building the "middle tier" of an application server, that is, the domain-specific objects in between the front end and the database/datastore. MiddleKit is roughly analogous to NeXT/Apple's Enterprise Objects and Sun's Enterprise Java Beans. '''
+synopsis = '''MiscUtils provides support classes and functions to Webware that aren't necessarily web-related and that don't fit into one of the other frameworks.'''
