7669f0f9c69e6ec3560c1b5048d3993ea6cb669b
Jay Love <jsliv@jslove.org>
2001-01-30 01:37:12 +0000

    Rename a bunch of files. New AppServer communication protocol. Fix fundamental logic error in AsyncThreadedAppServer.

--- WebKit/Adapter.py
+++ WebKit/Adapter.py
@@ -29,7 +29,7 @@ class Adapter(Configurable, Object):
 				'format': 'CGI',
 				'time':   time.time(),
 				'environ': env,
-				'input':   myInput
+			#	'input':   myInput
 				}
 
 		resp = ''
@@ -39,11 +39,16 @@ class Adapter(Configurable, Object):
 				# Send our request to the AppServer
 				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 				s.connect((host, port))
-				s.send(dumps(dict))
+				data = dumps(dict)
+				s.send(dumps(int(len(data))))
+				s.send(data)
+				s.send(myInput)
 				s.shutdown(1)
 
 				# Get the response from the AppServer
-				bufsize = 32*1024  # @@ 2000-04-26 ce: this should be configurable, also we should run some tests on different sizes
+				bufsize = 8*1024
+				# @@ 2000-04-26 ce: this should be configurable, also we should run some tests on different sizes
+				# @@ 2001-01-25 jsl: It really doesn't make a massive difference.  8k is fine and recommended.
 				resp = ''
 				while 1:
 					data = s.recv(bufsize)
@@ -61,5 +66,5 @@ class Adapter(Configurable, Object):
 					time.sleep(self.setting('SecondsBetweenRetries'))
 				else:
 					raise 'timed out waiting for app server to respond'
-		return loads(resp)
-
+		#return loads(resp)
+		return resp
