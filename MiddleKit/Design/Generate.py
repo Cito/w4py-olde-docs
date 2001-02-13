41fe7fa49974a788ad572f904abb1d4e603e7e66
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-02-13 05:20:34 +0000

    Improvs. Clean-up.

--- MiddleKit/Design/Generate.py
+++ MiddleKit/Design/Generate.py
@@ -11,56 +11,51 @@ from string import split, upper
 from getopt import getopt
 
 
-def FixPathForMiddleKit():
+def FixPathForMiddleKit(verbose=0):
+	'''
+	Enhances sys.path so that Generate.py can import MiddleKit.whatever.
+	We *always* enhance the sys.path so that Generate.py is using the MiddleKit that contains him, as opposed to whatever happens to be found first in the Python path. That's an subtle but important feature for those of us who sometimes have more than one MiddleKit on our systems.
+	'''
+	v = verbose
 	import os, sys
 	if globals().has_key('__file__'):
 		# We were imported as a module
 		location = __file__
+		if v: print 'took location from __file__'
 	else:
 		# We were executed directly
 		location = sys.argv[0]
+		if v: print 'took location from sys.argv[0]'
 
-	# location will basically be:
-	# .../MiddleKit/Design/Generate.py
-	v = 0  # verbose
 	if v: print 'location =', location
-	if location.find('MiddleKit')!=-1:
-		if v: print '>> MiddleKit in location'
-		index = location.index('MiddleKit')
-		location = location[:index]
-		if v: print 'new location =', location
-	location = os.path.abspath(os.path.normpath(location))
-	if v: print 'final location =', location
+	if location.lower()=='generate.py':
+		# The simple case. We're at MiddleKit/Design/Generate.py
+		location = os.path.abspath('../../')
+	else:
+		# location will basically be:
+		# .../MiddleKit/Design/Generate.py
+		if os.name=='nt':
+			# Case insenstive file systems:
+			location = location.lower()
+			what = 'middlekit'
+		else:
+			what = 'MiddleKit'
+		if location.find(what)!=-1:
+			if v: print 'MiddleKit in location'
+			index = location.index(what)
+			location = location[:index]
+			if v: print 'new location =', location
+		location = os.path.abspath(location)
+		if v: print 'final location =', location
 	sys.path.insert(1, location)
-	sys.path.insert(1, 'D:/cvs/Projects/Dev/')
 	if v: print 'path =', sys.path
 	if v: print
 	if v: print 'importing MiddleKit...'
 	import MiddleKit
 	if v: print 'done.'
 
-try:
-	import MiddleKit
-except:
-	FixPathForMiddleKit()
-
-# 2000-10-14 ce: DUPLICATED IN MiddleKit/Tests/Test.py
-def FixPathForConfigurable():
-	import os, sys
-	home = os.environ['HOME']
-	pjoin = os.path.join
-	norm = os.path.normpath
-	if len(sys.path) and sys.path[0]=='':
-		index = 1
-	else:
-		index = 0
-	sys.path.insert(index, norm(pjoin(home, 'Projects/Webware')))
-	sys.path.insert(index, norm(pjoin(home, 'Projects/Webware/WebKit')))
-
-try:
-	import Configurable
-except:
-	FixPathForConfigurable()
+FixPathForMiddleKit()
+import MiddleKit
 
 
 class Generate:
