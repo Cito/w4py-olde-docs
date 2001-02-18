23dc17a955418fdd1036986c0d873481353354f4
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-02-18 22:47:29 +0000

    Added NoDefault.

--- MiscUtils/__init__.py
+++ MiscUtils/__init__.py
@@ -5,6 +5,35 @@
 __all__ = ['Configurable', 'DBPool', 'DataTable', 'DictForArgs', 'Error', 'Funcs', 'MixIn', 'NamedValueAccess', 'PropertiesObject']
 
 
+class NoDefault:
+	'''
+	This provides a singleton "thing" which can be used to initialize
+	the "default=" arguments for different retrieval methods. For
+	example:
+
+		from MiscUtils import NoDefault
+		def bar(self, name, default=NoDefault):
+			if default is NoDefault:
+				return self._bars[name]  # will raise exception for invalid key
+			else:
+				return self._bars.get(name, default)
+
+	Consistently using a particular singleton is useful due to
+	subclassing considerations:
+
+		def bar(self, name, default=NoDefault):
+			if someCondition:
+				return self.specialBar(name)
+			else:
+				return SuperClass.bar(name, default)
+
+	It's also useful if one method that uses "default=NoDefault" relies
+	on another object and method to which it must pass the default.
+	(This is similar to the subclassing situation.)
+	'''
+	pass
+
+
 def InstallInWebKit(appServer):
 	pass
 
