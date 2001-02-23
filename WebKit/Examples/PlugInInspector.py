f01bd381a502cc136308b5f9dfa30137bc6684d3
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-02-23 19:24:36 +0000

    Fixed up.

--- WebKit/Examples/PlugInInspector.py
+++ WebKit/Examples/PlugInInspector.py
@@ -1,14 +1,19 @@
-from Page import Page
+from ExamplePage import ExamplePage
 from WebUtils.WebFuncs import HTMLEncode
-from types import *
+from types import StringType
 
 
-class PlugInInspector(Page):
-	def writeBody(self):
+class PlugInInspector(ExamplePage):
+	'''
+	This example is not public yet.
+	And this should probably just be axed and something
+	real added in Admin/PlugIns.
+	'''
+
+	def writeContent(self):
+		wr = self.writeln
 		for pi in self.application().server().plugIns():
-			for item in [pi, dir(pi), '__name__', '__file__', '__path__']:
-				if type(item) is StringType:
-					item = (item, getattr(pi, item))
-				self.writeln('<br> %s' % (HTMLEncode(str(item))))
+			wr('<b>%s</b>' % HTMLEncode(repr(pi)))
+			for item in dir(pi):
+				wr('<br> %s == %s' % (item, HTMLEncode(str(getattr(pi, item)))))
 			self.writeln('<hr>')
-
