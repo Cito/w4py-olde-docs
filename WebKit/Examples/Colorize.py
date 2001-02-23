89c3856b6abca3101587e1af5de7da6d731c0eb4
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-02-23 19:15:05 +0000

    Fixed problems with paths. Simplified.

--- WebKit/Examples/Colorize.py
+++ WebKit/Examples/Colorize.py
@@ -1,9 +1,10 @@
-from Page import Page
-import sys,os,string
+from WebKit.Page import Page
+import os, string, sys
 try:
 	from cStringIO import StringIO
 except:
 	from StringIO import StringIO
+
 
 class Colorize(Page):
 	"""
@@ -15,32 +16,18 @@ class Colorize(Page):
 		"""
 		write out a syntax hilighted version of the file.  The filename is an attribute of the request object
 		"""
-		res=transaction._response
-		req=self.request()
+		res = transaction._response
+		req = self.request()
 		if not req.hasField('filename'):
 			res.write("No filename given to syntax color!")
 			return
-		#filename = req.relativePath(req.field('filename')+'.py')
 		filename = req.field('filename')
 		if not os.path.exists(filename):
 			res.write(filename+" does not exist.")
 			return
 
-		try:
-			import py2html
-		except:
-			import imp
-			modinfo=imp.find_module('py2html',["DocSupport/",])
-			py2html=imp.load_module('py2html',modinfo[0],modinfo[1],modinfo[2])
-
-
-		try:
-			import PyFontify
-		except:
-			import imp
-			modinfo=imp.find_module('PyFontify',["DocSupport/",])
-			PyFontify=imp.load_module('PyFontify',modinfo[0],modinfo[1],modinfo[2])
-
+		from DocSupport import py2html
+		from DocSupport import PyFontify
 
 		myout=StringIO()
 		realout=sys.stdout
@@ -52,6 +39,4 @@ class Colorize(Page):
 		results = string.replace(results, '\t', '    ')  # 4 spaces per tab
 		res.write(results)
 
-		sys.stdout=realout
-
-
+		sys.stdout = realout
