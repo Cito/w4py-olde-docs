c528806e82ff42a341d874247f5e59b2b46858bc
Jay Love <jsliv@jslove.org>
2001-03-01 02:19:24 +0000

    delete old commented out code

--- WebKit/Examples/View.py
+++ WebKit/Examples/View.py
@@ -13,17 +13,5 @@ class View(ExamplePage):
 		if not req.hasField('filename'):
 			self.writeln('<p>', self.__class__.__doc__)
 		else:
-##			if req.hasField('tabSize'):
-##				tabSize = int(req.field('tabSize'))
-##			else:
-##				tabSize = 4
-##			filename = req.relativePath(req.field('filename')+'.py')
-##			contents = open(filename).read()
-##			if tabSize>0:
-##				contents = string.expandtabs(contents, tabSize)
-##			contents = string.replace(contents, '&', '&amp;')
-##			contents = string.replace(contents, '<', '&lt;')
-##			contents = string.replace(contents, '>', '&gt;')
-##			self.writeln('<br><i>%s</i><hr><pre>%s</pre>' % (filename, contents))
 			trans = self.transaction()
 			trans.application().forwardRequest(trans, "Colorize.py")
