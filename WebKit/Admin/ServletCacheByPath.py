77f06932de9c71270fb33b45de08500445ee05dc
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-03-21 18:37:13 +0000

    Substantial improvements to layout.

--- WebKit/Admin/ServletCacheByPath.py
+++ WebKit/Admin/ServletCacheByPath.py
@@ -1,7 +1,7 @@
 from AdminSecurity import AdminSecurity
-from string import join
 from Queue import Queue
 from WebUtils.Funcs import htmlEncode
+import os, string, time
 
 
 class ServletCacheByPath(AdminSecurity):
@@ -9,8 +9,8 @@ class ServletCacheByPath(AdminSecurity):
 	This servlet displays, in a readable form, the internal data
 	structure of the application known as the "servlet cache by path".
 
-	This can be useful for debugging WebKit problems and interesting
-	in general.
+	This can be useful for debugging WebKit problems and the
+	information is interesting in general.
 	'''
 
 	def title(self):
@@ -21,29 +21,63 @@ class ServletCacheByPath(AdminSecurity):
 		self.writeln(htCache(cache))
 
 
+def sortSplitFilenames(a, b):
+	''' This is a utility function for list.sort() that handles list elements that come from os.path.split. We sort first by base filename and then by directory, case insensitive. '''
+	result = cmp(string.lower(a['base']), string.lower(b['base']))
+	if result==0:
+		result = cmp(string.lower(a['dir']), string.lower(b['dir']))
+	return result
+
+
 def htCache(cache):
 	html = []
-	ap = html.append
+	wr = html.append
 
 	keys = cache.keys()
 	keys.sort()
 
-	ap('%d paths' % len(keys))
-	ap('<table align=center border=1 cellpadding=2 cellspacing=2>\n')
+	wr('%d unique paths exist in the servlet cache.' % len(keys))
 
+	wr('<p> Click any link to jump to the details for that path.')
+
+	wr('<p> Filenames:')
+	wr('<table>')
+	wr('<tr> <td> File </td> <td> Directory </td> </tr>')
+	paths = []
 	for key in keys:
-		ap('<tr> <td> %s </td> <td>' % key)
-		ap(htRecord(cache[key]))
-		ap('</td> </tr>')
-	ap('</table>')
+		dir, base = os.path.split(key)
+		path = { 'dir': dir, 'base': base, 'full': key }
+		paths.append(path)
+	paths.sort(sortSplitFilenames)
+	# At this point, paths is a list where each element is a tuple
+	# of (basename, dirname, fullPathname) sorted first by basename
+	# and second by dirname
+	for path in paths:
+		wr('<tr> <td> <a href=#%s>%s</a> </td> <td> %s </td> </tr>\n' % (id(path['full']), path['base'], path['dir']))
+	wr('</table>')
 
-	return join(html, '')
+	wr('<p> Full paths:')
+	for key in keys:
+		wr('<br> <a href=#%s>%s</a>\n' % (id(key), key))
+
+	wr('<p> Details:')
+	wr('<table align=center border=0 cellpadding=2 cellspacing=2>\n')
+
+	spacer = '&nbsp;'*8
+	for path in paths:
+		wr('<tr> <td colspan=2> <br> <a name=%s><strong>%s</strong> - %s</a> </td> </tr>\n' % (id(path['full']), path['base'], path['dir']))
+		wr('<tr> <td nowrap>%s</td> <td> ' % spacer)
+		wr(htRecord(cache[path['full']]))
+		wr('</td> </tr>')
+	wr('</table>')
+
+	return string.join(html, '')
 
 
 def htRecord(record):
 	html = []
-	ap = html.append
-	ap('<table border=1 width=100%>')
+	wr = html.append
+	wr('<table border=0 width=100%>')
 	keys = record.keys()
 	keys.sort()
 	for key in keys:
@@ -52,19 +86,24 @@ def htRecord(record):
 		# determine the HTML for the value
 		value = record[key]
 		htValue = None
+
+		# check for special cases where we want a custom display
 		if hasattr(value, '__class__'):
-			# check for special cases where we want a custom display
 			if issubclass(value.__class__, Queue):
 				htValue = htQueue(value)
+		if key=='timestamp':
+			htValue = '%s (%s)' % (time.asctime(time.localtime(value)), str(value))
+
+		# the general case:
 		if not htValue:
-			# the default is just the str() of the value
 			htValue = htmlEncode(str(value))
 
-		ap('<tr> <td> %s </td> <td> %s </td> </tr>' % (htKey, htValue))
-	ap('</table>')
-	return join(html, '')
+		wr('<tr> <td bgcolor=#EEEEEE> %s </td> <td bgcolor=#EEEEEE> %s </td> </tr>' % (htKey, htValue))
+	wr('</table>')
+	return string.join(html, '')
 
 
 def htQueue(queue):
+	# @@ 2002-03-21 ce: Could probably do something nicer here in the
+	# future, like put a <br> in between each element of the queue.
 	return 'Queue: ' + htmlEncode(str(queue.queue))
-	
