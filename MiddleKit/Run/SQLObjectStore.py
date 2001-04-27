f35e57cd77a84d4611d001099f0c65850a4d4786
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-04-27 20:28:20 +0000

    Assertion that instantiated objects are middle objects. Leads to a more understandable exception.

--- MiddleKit/Run/SQLObjectStore.py
+++ MiddleKit/Run/SQLObjectStore.py
@@ -186,6 +186,7 @@ class SQLObjectStore(ObjectStore):
 					exec 'from %s import %s' % (className, className) in results
 					pyClass = results[className]
 					obj = pyClass()
+					assert isinstance(obj, MiddleObject), 'Not a MiddleObject. obj = %r, type = %r, MiddleObject = %r' % (obj, type(obj), MiddleObject)
 					obj._mk_store = self
 					# ^^^ a store is the only outsider that is allowed
 					# to directly set a MiddleObject's store like this
