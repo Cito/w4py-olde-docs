2ed38977736b452398a4822331f397e65040b1d0
Jay Love <jsliv@jslove.org>
2001-05-14 01:15:18 +0000

    use includeServlet

--- WebKit/Examples/Forward.py
+++ WebKit/Examples/Forward.py
@@ -5,5 +5,6 @@ class Forward(ExamplePage):
 	def writeBody(self):
 		trans = self.transaction()
 		resp = self.response()
-		resp.write("<p>This is the Forward servlet speaking. I am now going to forward the request to the Welcome servlet via Application's forwardRequest() method:<br><p>")
-		trans.application().forwardRequest(trans, 'Welcome.py')
+		resp.write("<p>This is the Forward servlet speaking. I am now going to include the output of the Welcome servlet via Application's includeServlet() method:<br><p>")
+#		trans.application().forwardRequest(trans, 'Welcome.py')
+		trans.application().includeServlet(trans, 'Welcome.py')
