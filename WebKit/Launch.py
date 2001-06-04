419d9eef7fa687852d7e350b98b5e40045379922
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-06-04 13:57:31 +0000

    Path handling revamp. See update webware-devel for today.

--- WebKit/Launch.py
+++ WebKit/Launch.py
@@ -3,7 +3,6 @@
 import os, sys
 
 def usage():
-#	sys.stdout = sys.stderr
 	print 'error: Launch.py (of WebKit)'
 	print 'usage: Launch.py SERVER ARGS'
 	sys.exit(1)
@@ -11,35 +10,39 @@ def usage():
 def main(args):
 	if len(args)<2:
 		usage()
-	server = args[1]
+
+	server = args[1] # the server
+
+	# allow for a .py on the server name
 	if server[-3:]=='.py':
 		server = server[:-3]
-		
-	if '' not in sys.path:   ##'' is the directory this file is in
-		sys.path.insert(0,'')
-	try:
-		import WebwarePathLocation
-		wwdir = os.path.abspath(os.path.join(os.path.dirname(WebwarePathLocation.__file__),".."))
-	except Exception, e:
-		print e
-		usage()
 
-	try:
-		sys.path.remove('.')
-	except:
-		pass
-	
-	sys.path.remove('')
-	
-	if not wwdir in sys.path:
-		sys.path.insert(0,wwdir)
+	# figure out directory locations
+	webKitDir = os.path.dirname(os.path.join(os.getcwd(), sys.argv[0]))
+	webwareDir = os.path.dirname(webKitDir)
+
+	# clean up sys.path
+	def ok(directory):
+		return directory not in ['', '.'] and directory[-6:].lower()!='webkit'
+	sys.path = filter(ok, sys.path)
+	#if not webwareDir in sys.path:
+	#	sys.path.insert(0, webwareDir)
+	sys.path.insert(0, '')
+
+	# go to Webware dir so that:
+	#   - importing packages like 'WebUtils' hits this Webware
+	#   - we cannot import WebKit modules without qualifying them
+	os.chdir(webwareDir)
+
+	# Import the server's main()
 	import WebKit
 	code = 'from WebKit.%s import main' % server
 	exec code
 	args = args[2:]
-	
+
+	# Run!
 	main(args)
 
-	
+
 if __name__=='__main__':
 	main(sys.argv)
