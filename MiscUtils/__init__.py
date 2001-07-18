e6bbf6c1acd5bb32d3437540ce718b12d8e943de
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-07-18 21:01:55 +0000

    added doc string to SubclassResponsibilityError

--- MiscUtils/__init__.py
+++ MiscUtils/__init__.py
@@ -5,6 +5,27 @@
 __all__ = ['Configurable', 'DBPool', 'DataTable', 'DictForArgs', 'Error', 'Funcs', 'MixIn', 'NamedValueAccess', 'PropertiesObject', 'unittest']
 
 
+class SubclassResponsibilityError(NotImplementedError):
+	'''
+	This exception is raised by abstract methods in abstract classes. It
+	is a special case of NotImplementedError, that indicates that the
+	implementation won't be provided at that location in the future
+	--instead the subclass should provide it.
+
+	Typical usage:
+
+	from MiscUtils import SubclassResponsibilityError
+
+	class Foo:
+		def bar(self):
+			raise SubclassResponsibilityError, self.__class__
+
+	Note that added the self.__class__ makes the resulting exception
+	much more useful.
+	'''
+	pass
+
+
 class NoDefault:
 	'''
 	This provides a singleton "thing" which can be used to initialize
