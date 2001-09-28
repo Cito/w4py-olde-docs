0afeeae3df8ba147ecc76f50c44fe8e8f3c0e2da
Geoffrey Talvola <gtalvola@nameconnector.com>
2001-09-28 03:32:06 +0000

    adding test for DeleteBehavior = mark

--- MiddleKit/Tests/Test.py
+++ MiddleKit/Tests/Test.py
@@ -42,7 +42,7 @@ class Test:
 		if len(args)>1:
 			self._modelNames = args[1:]
 		else:
-			self._modelNames = 'MKBasic MKNone MKString MKDateTime MKDefaultMinMax MKTypeValueChecking MKInheritance MKInheritanceAbstract MKList MKObjRef MKObjRefReuse MKDelete'.split()
+			self._modelNames = 'MKBasic MKNone MKString MKDateTime MKDefaultMinMax MKTypeValueChecking MKInheritance MKInheritanceAbstract MKList MKObjRef MKObjRefReuse MKDelete MKDeleteMark'.split()
 
 	def testEmpty(self):
 		self.testRun('TestEmpty.py', deleteData=0)
