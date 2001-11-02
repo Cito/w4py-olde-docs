29db9028268bf4a0761bb92f552c3b0049b4292d
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-11-02 20:48:59 +0000

    fixed die() for Python 2.2

--- WebKit/Transaction.py
+++ WebKit/Transaction.py
@@ -117,7 +117,7 @@ class Transaction(Object):
 	def die(self):
 		''' This method should be invoked when the entire transaction is finished with. Currently, this is invoked by AppServer. This method removes references to the different objects in the transaction, breaking cyclic reference chains and allowing either older versions of Python to collect garbage, or newer versions to collect it faster. '''
 		from types import InstanceType
-		for attrName in dir(self):
+		for attrName in self.__dict__.keys():
 			# @@ 2000-05-21 ce: there's got to be a better way!
 			attr = getattr(self, attrName)
 			if type(attr) is InstanceType and hasattr(attr, 'resetKeyBindings'):
