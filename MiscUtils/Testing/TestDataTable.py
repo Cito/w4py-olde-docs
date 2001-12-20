81cb94ca37e3470f101de5a5ad70b9b8c4f2c2e8
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2001-12-20 20:01:11 +0000

    test for comments; minor code clean

--- MiscUtils/Testing/TestDataTable.py
+++ MiscUtils/Testing/TestDataTable.py
@@ -1,13 +1,31 @@
-import sys
-sys.path.insert(0, '..')
+import FixPath
 from DataTable import *
 import string
+from StringIO import StringIO
+
 
 # @@ 2000-12-04 ce: We don't test the feature where record like objects, that respond to hasValueForKey() and valueForKey(), can be added to a table (as opposed to a sequence, dictionary or TableRecord instance).
 
 def heading(title):
-	print
-	print title
+	print 'Testing %s...' % title
+
+
+def testSource(name, src, headings, data):
+	heading(name+':')
+	dt = DataTable()
+	lines = split(src, '\n')
+	dt.readLines(lines)
+	assert [col.name() for col in dt.headings()]==headings
+	i = 0
+	while i<len(dt):
+		match = data[i]
+		if dt[i].asList()!=match:
+			print 'mismatch'
+			print 'i      :', i
+			print 'match  :', match
+			print 'dt[i]  :', dt[i]
+			raise AssertionError
+		i = i + 1
 
 
 def test01():
@@ -38,7 +56,15 @@ def test01():
 	print t
 
 	heading('Writing file (CSV):')
-	t.writeFile(sys.stdout)
+	answer = '''\
+name,age,rating
+John,26,7.2
+Mary,32,8.3
+'''
+	out = StringIO()
+	t.writeFile(out)
+	results = out.getvalue()
+	assert results==answer, '\n%r\n%r\n' % (results, answer)
 
 	heading('Accessing rows:')
 	for row in t:
@@ -55,25 +81,20 @@ def test01():
 	t.append([4, 5, 6])
 	assert t[0]['x'] - t[1]['z'] == -5
 
-	heading('Quoted values:')
-	t = DataTable()
-	lines = split('''"x","y,y",z
-		a,b,c
-		a,b,"c,d"
-		"a,b",c,d
-		"a","b","c"
-		"a",b,"c"
-		"a,b,c"
-		"","",""
-		"a","",
-''', '\n')
-
-	t.readLines(lines)
-	assert t.headings()[0].name()=='x'
-	assert t.headings()[1].name()=='y,y'
-	assert t.headings()[2].name()=='z'
-
-	matches = [
+	# Basics
+	src = '''\
+"x","y,y",z
+a,b,c
+a,b,"c,d"
+"a,b",c,d
+"a","b","c"
+"a",b,"c"
+"a,b,c"
+"","",""
+"a","",
+'''
+	headings = ['x', 'y,y', 'z']
+	data = [
 		['a', 'b', 'c'],
 		['a', 'b', 'c,d'],
 		['a,b', 'c', 'd'],
@@ -83,16 +104,31 @@ def test01():
 		['', '', ''],
 		['a', '', '']
 	]
-	i = 0
-	while i<len(t):
-		match = matches[i]
-		if t[i].asList()!=match:
-			print 'mismatch'
-			print 'i     :', i
-			print 'match :', match
-			print 't[i]  :', t[i]
-			raise AssertionError
-		i = i + 1
+	testSource('Basics', src, headings, data)
+
+
+	# Comments
+	src = '''\
+a:int,b:int
+1,2
+#3,4
+5,6
+'''
+	headings = ['a', 'b']
+	data = [
+		[1, 2],
+		[5, 6],
+	]
+	testSource('Comments', src, headings, data)
+
+	# Multiline records
+	src = '''\
+a
+"""Hi""",
+'''
+	headings = ['a']
+	data = ['"Hi"']
+	#testSource('Multiline records', src, headings, data)
 
 	print
 
