30a040a03b36375f842abfb076efbae027c2b347
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2002-01-07 00:07:46 +0000

    added joinCSVFields()

--- MiscUtils/CSVParser.py
+++ MiscUtils/CSVParser.py
@@ -242,3 +242,24 @@ class CSVParser:
 # Call the global function parse() if you like the default settings of the CSVParser
 _parser = CSVParser()
 parse = _parser.parse
+
+
+import types
+def joinCSVFields(fields):
+	'''
+	Returns a CSV record (eg a string) from a sequence of fields.
+	Fields containing commands (,) or double quotes (") are quoted
+	and double quotes are escaped (""). The terminating newline is
+	NOT included.
+	'''
+	newFields = []
+	for field in fields:
+		assert type(field) is types.StringType
+		if field.find('"')!=-1:
+			newField = '"' + field.replace('"', '""') + '"'
+		elif field.find(',')!=-1:
+			newField = '"' + field + '"'
+		else:
+			newField = field
+		newFields.append(newField)
+	return ','.join(newFields)
