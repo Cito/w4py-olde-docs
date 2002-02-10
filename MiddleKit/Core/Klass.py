8b3318a44930569a0f4d6620b379f50d9265e3a9
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2002-02-10 21:00:25 +0000

    fixed a bug in isKindOfKlassNamed() for case where return value should be false

--- MiddleKit/Core/Klass.py
+++ MiddleKit/Core/Klass.py
@@ -132,7 +132,7 @@ class Klass(UserDict, ModelObject):
 		if self.name()==name:
 			return 1
 		else:
-			return self.lookupAncestorKlass(name) is not None
+			return self.lookupAncestorKlass(name, None) is not None
 
 
 	## Subklasses ##
