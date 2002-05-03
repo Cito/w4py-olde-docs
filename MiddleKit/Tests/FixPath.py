1e1bb7f2404a5851359fffe06dc9138cebeb1881
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2002-05-03 00:34:56 +0000

    always guarantee that we're testing the local MiddleKit and not some other!

--- MiddleKit/Tests/FixPath.py
+++ MiddleKit/Tests/FixPath.py
@@ -1,17 +1,17 @@
-'''
+"""
 FixPath.py
-'''
+
+Enhance sys.path so that we're guaranteed to import the MiddleKit that
+we reside in. This is particularly important for Webware developers
+that might have more than one Webware source tree on their file system.
+"""
 
 
-def FixPathForMiddleKit():
-	''' If MiddleKit can't be imported, this method will be invoked to augment the path.
-	@@ 2001-02-02 ce: Should we always enhance the sys.path to make sure that the tests hit the MiddleKit they belong to and not some other MiddleKit?
-	'''
-	# We're located at .../MiddleKit/Run/Tests/Test.py.
-	import os, sys
-	if len(sys.path) and sys.path[0]=='':
-		index = 1
-	else:
-		index = 0
-	sys.path.insert(index, os.path.abspath('../..'))
-	import MiddleKit
+# We're located at .../MiddleKit/Run/Tests/Test.py.
+import os, sys
+if sys.path and sys.path[0]=='':
+	index = 1
+else:
+	index = 0
+sys.path.insert(index, os.path.abspath('../..'))
+import MiddleKit
--- MiddleKit/Tests/TestCommon.py
+++ MiddleKit/Tests/TestCommon.py
@@ -6,17 +6,7 @@ This is just a convenience module for the various test modules (TestFoo.py).
 
 
 import os, string, sys, time
-
-
-from FixPath import *
-
-try:
-	import MiddleKit
-except ImportError:
-	FixPathForMiddleKit()
-	import MiddleKit
-
+import FixPath
 import MiscUtils
-
-
+import MiddleKit
 from MiddleKit.Core.Klasses import Klasses
