e2fc97577eda7eabce1d8439594b8f9cbd0f6c68
Dave Rogers <dave@drogers.us>
2002-08-08 19:35:52 +0000

    synchronized with latest middlekit conventions

--- MiddleKit/Run/MSSQLObjectStore.py
+++ MiddleKit/Run/MSSQLObjectStore.py
@@ -1,9 +1,9 @@
-#import SQLObjectStore
-#reload (SQLObjectStore)
 from SQLObjectStore import SQLObjectStore
-import ODBC.Windows
+from mx import ODBC # DR: 07-12-02 The ODBC.Windows module is flawed
+ODBC.Windows.threadsafety = ODBC.Windows.threadlevel # mx.ODBC.Windows has a threadlevel, not a threadsafety, even though DBABI2.0 says its threadsafety (http://www.python.org/topics/database/DatabaseAPI-2.0.html)
 
 class MSSQLObjectStore(SQLObjectStore):
+	_threadSafety = ODBC.Windows.threadsafety
 	"""
 	MSSQLObjectStore does the obvious: it implements an object store backed by a MSSQL database.
 
@@ -29,9 +29,16 @@ class MSSQLObjectStore(SQLObjectStore):
 		return apply(ODBC.Windows.Connect, (), self._dbArgs)
 
 	def retrieveLastInsertId(self, conn, cur):
-		result = self.executeSQL('select @@IDENTITY', conn)
-		return int(self._cursor.fetchone()[0])
+		conn, cur = self.executeSQL('select @@IDENTITY', conn)
+		return int(cur.fetchone()[0])
 
+	def newConnection(self):
+		args = self._dbArgs.copy()
+#		args['database'] = self._model.sqlDatabaseName()
+		return self.dbapiModule().connect(**args)
+
+	def dbapiModule(self):
+		return ODBC.Windows
 
 class Klass:
 
@@ -40,7 +47,15 @@ class Klass:
 		Returns "[name]" so that table names do not conflict with SQL
 		reserved words.
 		"""
-		return '[%s]' % self.sqlTableName()
+		return '[%s]' % self.name()
+
+class Attr:
+
+	def sqlColumnName(self):
+		""" Returns the SQL column name corresponding to this attribute, consisting of self.name() + self.sqlTypeSuffix(). """
+		if not self._sqlColumnName:
+			self._sqlColumnName = self.name() + self.sqlTypeSuffix()
+		return '[' + self._sqlColumnName + ']'
 
 
 class StringAttr:
