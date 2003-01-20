aeccd4d260fb7df125f26e4e839907216ca9c259
Stuart  Donaldson <stu@asyn.com>
2003-01-20 18:51:22 +0000

    Updated to use "cvs export" by default.  Has the benefit of making it simpler to verify that what is in the released tarball is only what came from CVS, and does not include local working directory changes that may not have been committed, or files that may have been added without being placed in CVS.
    Takes a CVS tag as an optional argument that will allow exporting
    the specific tag.  This is the method to use to do a full release.
    Snapshots will be the default with a datestamp.
    
    If you want the old behavior, using an updated current directory
    pass the argument "method=current".

--- bin/ReleaseHelper.py
+++ bin/ReleaseHelper.py
@@ -31,11 +31,20 @@ import os, sys, time
 
 
 class ReleaseHelper:
-
+	def __init__(self):
+		self.args = { 'method' : 'export'
+			}
 	def main(self):
 		self.writeHello()
 		self.checkPlatform()
 		self.readArgs()
+
+		if self.args.get("method",None) == "export":
+			return self.release_export()
+		
+		self.release_current()
+
+	def release_current(self):
 
 		progPath = os.path.join(os.getcwd(), sys.argv[0])  # the location of this script
 		webwarePath = os.path.dirname(os.path.dirname(progPath))  # because we're in Webware/bin/
@@ -77,6 +86,103 @@ class ReleaseHelper:
 
 		self.writeGoodBye(locals())
 
+	def release_export(self):
+		"""
+		Prepare a release by using the cvs export approach.  This means the release will
+		match exactly what is in CVS, and reduces the risk of local changes, modified
+		files, or new files which are not in CVS from showing up in the release.
+
+		Furthermore, this takes the version number from the checked out information.
+		This means it is possible to specify a tag=cvstag on the command line and
+		build a release associated with that tag.
+
+		    > ReleaseHelper.py [tag=cvstag]
+
+	        So specifying a CVS tag such as Release-0_7 will export the CVS files tagged
+		with Release-0_7 and build Webware-0.7.tar.gz
+		"""
+		
+		self.writeHello()
+		self.checkPlatform()
+		self.readArgs()
+
+		progPath = os.path.join(os.getcwd(), sys.argv[0])  # the location of this script
+		webwarePath = os.path.dirname(os.path.dirname(progPath))  # because we're in Webware/bin/
+		parentPath = os.path.dirname(webwarePath)  # where the tarball will land
+
+		self.chdir(webwarePath)
+		from MiscUtils.PropertiesObject import PropertiesObject
+
+		if self.args.has_key('tag'):
+			dtag = '-r ' + self.args['tag']
+			datestamp = ''
+		else:
+			# timestamp for tomorrow to insure we get latest data off
+			# of the trunk.  Is this needed?
+			year,month,day = time.gmtime(time.time()+3600*24)[:3]  
+			dtag = '-D %04d-%02d-%02d' % (year,month,day)
+
+			# timestamp for time of release used to in versioning the file.
+			year,month,day = time.localtime(time.time())[:3]  
+			datestamp = "%04d%02d%02d" % (year,month,day)
+
+			# drop leading 2 digits from year. (Ok, itn's not Y2K but it is short
+			# and unique in a narrow time range of 100 years.)
+			datestamp = datestamp[2:]
+			
+			print "No cvs tag specified, assuming snapshot of CVS."
+			
+		tempName = "ReleaseHelper-Export"
+		if os.path.exists( tempName ):
+			print "There is incomplete ReleaseHelper data in:", tempName
+			print "Please remove this directory."
+			return 1
+		
+		cleanup = [ tempName ]
+		
+		try:
+			self.run('cvs -z3 export %s -d %s Webware' % (dtag, tempName))
+			props = PropertiesObject(os.path.join(webwarePath, tempName, 'Properties.py'))
+			ver = props['versionString']
+
+			print "Webware version is:", ver
+			if datestamp:
+				ver = ver + "-" + datestamp
+				print "Packaged release will be:", ver
+
+			pkgDir = "Webware-%s" % ver
+			
+			if os.path.exists(pkgDir):
+				print "** %s is in the way, please remove it." % pkgDir
+				return
+
+			# rename the tempName to the pkgDir so the extracted parent
+			# directory from the tarball will be unique to this package.
+			self.run("mv %s %s" % (tempName, pkgDir))
+			
+			cleanup.append( pkgDir )
+			
+			pkgName = os.path.join(parentPath, pkgDir + ".tar.gz")
+
+			# cleanup .cvs files
+			self.run("find %s -name '.cvs*' -exec rm {} \;" % pkgDir)
+
+			# cleanup any other files not part of this release. (ie: documentation?)
+
+			# build any internal documentation for release.
+			
+			self.run('tar czf %s %s' % (pkgName, pkgDir))
+
+			assert os.path.exists(os.path.join(parentPath, pkgName))
+
+		finally:
+			# Clean up
+			for path in cleanup:
+				if os.path.exists(path):
+					self.run('rm -rf %s' % os.path.join(webwarePath, path))
+			 
+		self.writeGoodBye(locals())
+
 	def writeHello(self):
 		print 'Webware for Python'
 		print 'Release Helper'
@@ -90,7 +196,7 @@ class ReleaseHelper:
 			self.error()
 
 	def readArgs(self):
-		args = {}
+		args = self.args.copy()     # initialize with default.
 		for arg in sys.argv[1:]:
 			try:
 				name, value = arg.split('=')
