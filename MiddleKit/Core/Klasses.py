b72321b6ef472317c91d3f74b053cc30df28fb1c
Chuck Esterbrook <chuckesterbrook@yahoo.com>
2003-12-19 05:20:19 +0000

    - add new error checking that a model is not empty

--- MiddleKit/Core/Klasses.py
+++ MiddleKit/Core/Klasses.py
@@ -77,6 +77,8 @@ class Klasses(ModelObject, UserDict):
 		# @@ 2000-11-24 ce: split into readTable()
 		self._filename = filename
 		table = DataTable(filename, usePickleCache=0)  # because PickleCache is used at the Model level
+		if len(table)<1:
+			raise ModelError('Class model appears empty.')
 		# in case we want to look at these later:
 		self._tableHeadings = table.headings()
 
@@ -154,7 +156,7 @@ class Klasses(ModelObject, UserDict):
 				raise ModelError("no type specified for attribute '%s'" % dict['Attribute'])
 			else:
 				raise ModelError('type specifier missing')
-			
+
 
 		if typeName[0].upper()==typeName[0]:
 			return 'ObjRefAttr'
