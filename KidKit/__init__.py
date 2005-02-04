78a304aec712f6c546187af3583e8853f8121d78
Winston Wolff <flapjack@users.sourceforge.net>
2005-02-04 01:52:50 +0000

    Loading of KidKit plugin now fails gracefully if 'kid' cannot be imported.

--- KidKit/__init__.py
+++ KidKit/__init__.py
@@ -1,7 +1,13 @@
 # Webware for Python
 
-from KidServletFactory import KidServletFactory
-
 def InstallInWebKit( appServer ):
+	try:
+		import kidx
+	except:
+		print '\tCould not import kid library.  Not loading KidKit.'
+		return
+	
+	from KidServletFactory import KidServletFactory
+
 	app = appServer.application()
 	app.addServletFactory( KidServletFactory(app) )
