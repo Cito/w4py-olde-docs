032c5e10eb552239b35f286950a05b774d1e6050
Christoph Zwerschke <cito@online.de>
2005-09-18 17:17:35 +0000

    Improved the AppServer scripts for Unix and Windows

--- WebKit/AppServer.bat
+++ WebKit/AppServer.bat
@@ -1,4 +1,26 @@
 @echo off
+
+rem WebKit application server launch script for Windows.
+
+rem You may give the following Python parameters in advance,
+rem followed by the parameters passed on to ThreadedAppServer:
+rem   -O with optimization (.pyo instead of .pyc)
+rem   -u unbuffered output (useful for debugging)
+set OPTIONS=
+:getopt
+if "%1"=="-O" goto setopt
+if "%1"=="-u" goto setopt
+goto continue
+:setopt
+set OPTIONS=%OPTIONS% %1
+shift
+goto getopt
+:continue
+
+rem Make the directory where this script lives the current directory:
+if exist $~dpn0 cd /d %~dp0
+
+rem As long as the app server returns a 3, it wants to be restarted:
 :restart
-python Launch.py ThreadedAppServer %1 %2 %3 %4 %5
+python Launch.py %OPTIONS% ThreadedAppServer %1 %2 %3 %4 %5
 if errorlevel 3 goto restart
