1b8eb58e4d54b654eea3c921a9873089dad00a50
Christoph Zwerschke <cito@online.de>
2005-09-19 17:50:08 +0000

    The AppServer sh script tried to be too smart - simplified it. Renamed OPTIONS to PY_OPTS to make clear which options are meant.

--- WebKit/AppServer.bat
+++ WebKit/AppServer.bat
@@ -6,13 +6,13 @@ rem You may give the following Python parameters in advance,
 rem followed by the parameters passed on to ThreadedAppServer:
 rem   -O with optimization (.pyo instead of .pyc)
 rem   -u unbuffered output (useful for debugging)
-set OPTIONS=
+set PY_OPTS=
 :getopt
 if "%1"=="-O" goto setopt
 if "%1"=="-u" goto setopt
 goto continue
 :setopt
-set OPTIONS=%OPTIONS% %1
+set PY_OPTS=%PY_OPTS% %1
 shift
 goto getopt
 :continue
@@ -22,5 +22,5 @@ if exist $~dpn0 cd /d %~dp0
 
 rem As long as the app server returns a 3, it wants to be restarted:
 :restart
-python %OPTIONS% Launch.py ThreadedAppServer %1 %2 %3 %4 %5
+python%PY_OPTS% Launch.py ThreadedAppServer %1 %2 %3 %4 %5 %6 %7 %8 %9
 if errorlevel 3 goto restart
