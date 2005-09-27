22e7accb1a3f7b6de720a6a552245232fa50d41d
Christoph Zwerschke <cito@online.de>
2005-09-27 22:43:53 +0000

    Made the Image example work with PIL as well, if GD is not installed.

--- WebKit/Examples/Image.py
+++ WebKit/Examples/Image.py
@@ -1,19 +1,31 @@
 from WebKit.HTTPContent import HTTPContent
 try:
-	import gd
+	import gd # GD module
 except ImportError:
 	gd = None
+	try:
+		import PIL # Python Imaging Library
+		import PIL.Image
+		import PIL.ImageDraw
+		pil = PIL
+	except ImportError:
+		pil = None
+
 
 class Image(HTTPContent):
 	"""Dynamic image generation example.
 
+	This example creates an image of a sinusoid.
+
 	For more information on generating graphics, see
 	http://python.org/topics/web/graphics.html.
+
+	This example works with both PIL and GD.
 
 	"""
 
 	def defaultAction(self):
-		if gd:
+		if gd or pil:
 			image = self.generatePNGImage()
 			res = self.response()
 			res.setHeader("Content-Type", "image/png")
@@ -26,7 +38,8 @@ class Image(HTTPContent):
 			self.writeln('<html><head><title>Image Demo</title>'
 				'<body style="background-color:white;padding:16;font-family:sans-serif">'
 				'<h2>WebKit Image Generation Demo</h2>'
-				'<h4 style="color:red">Sorry: This page requires the Python GD module.</h4>'
+				'<h4 style="color:red">Sorry:'
+					' This page requires the Python Imaging Library or the GD module.</h4>'
 				'</p>See <a href="http://python.org/topics/web/graphics.html">'
 				'python.org/topics/web/graphics.html</a>.</p></body></html>')
 
@@ -35,23 +48,38 @@ class Image(HTTPContent):
 		import StringIO
 		from math import sin, pi
 		X, Y = (320, 160)
-		im = gd.image((X,Y))
-		white = im.colorAllocate((255, 255, 255))
-		black = im.colorAllocate((0, 0, 0))
-		blue = im.colorAllocate((0, 0, 255))
-		im.colorTransparent(white)
-		im.string(gd.gdFontLarge, (int(X*0.45), int(Y*0.1)), "y=sin(x)", black)
-		# map co-ordinates: (x=0..2pi, y=-1..1) => (0..X, Y..0)
-		def T(x,y):
-			return (int((x/(2*pi))*X), int(Y - ((y+1)/2.0)*Y))
-		im.line(T(0,0), T(2*pi,0), black) # x-axis
-		im.line(T(0,-1), T(0,1), black) # y-axis
-		points = []
-		x = 0
-		while x < 2*pi:
-			points.append(T(x,sin(x)))
-			x += 0.1
-		im.lines(points, blue)
+		def T(p):
+			# map coordinates: (x=0..2pi, y=-1..1) => (0..X, Y..0)
+			return (int((p[0]/(2*pi))*X), int(Y - ((p[1]+1)/2.0)*Y))
+		white, black, blue = (255, 255, 255), (0, 0, 0), (0, 0, 255)
+		if gd:
+			im = gd.image((X, Y))
+			white, black, blue = map(im.colorAllocate, (white, black, blue))
+			font = gd.gdFontLarge
+		else:
+			im = pil.Image.new('RGB', (X, Y), white)
+			draw = pil.ImageDraw.Draw(im)
+		def text(pos, string, color):
+			pos = T(pos)
+			if gd:
+				im.string(font, pos, string, color)
+			else:
+				draw.text(pos, string, color)
+		text((2.7, 0.8), 'y=sin(x)', black)
+		def lines(points, color):
+			points = map(T, points)
+			if gd:
+				im.lines(points, color)
+			else:
+				draw.line(points, color)
+		lines(((0,0), (2*pi, 0)), black) # x-axis
+		lines(((0,-1), (0,1)), black) # y-axis
+		def f(x): # sin function
+			return (x, sin(x))
+		lines(map(f, map(lambda x: x*2*pi/X, xrange(X+1))), blue)
 		f = StringIO.StringIO()
-		im.writePng(f)
+		if gd:
+			im.writePng(f)
+		else:
+			im.save(f, 'png')
 		return f.getvalue()
