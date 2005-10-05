32facf67eb9a834b716e891e3a26579e509875be
Christoph Zwerschke <cito@online.de>
2005-10-05 13:54:42 +0000

    Added adapter to the urls in the examples welcome page.

--- WebKit/Examples/Welcome.py
+++ WebKit/Examples/Welcome.py
@@ -4,6 +4,7 @@ class Welcome(ExamplePage):
 
 	def writeContent(self):
 		self.writeln('<h2>Welcome to WebKit %s!</h2>' % self.application().webKitVersionString())
+		adapter = self.request().adapterName()
 		self.writeln('''\
 		<p> Along the side of this page you will see various links that will take you to:</p>
 		<ul>
@@ -14,13 +15,13 @@ class Welcome(ExamplePage):
 				usually given a descriptive name.</li>
 			<li>External sites, such as the Webware home page.</li>
 		</ul>
-		<p>The <a href="../Admin/">Admin</a> context is particularly interesting because
+		<p>The <a href="%(adapter)s/Admin/">Admin</a> context is particularly interesting because
 		it takes you to the administrative pages for the WebKit application server where
 		you can review logs, configuration, plug-ins, etc.</p>
-		<p>The <a href="../Docs/">Docs</a> contexts allow you to browse
-		the documentation of <a href="../WebKit/Docs/">WebKit</a>
-		and <a href="../Docs/ComponentIndex.html">all other components</a>
-		of <a href="../Docs/">Webware for Python</a>.</p>''')
+		<p>The <a href="%(adapter)s/Docs/">Docs</a> contexts allow you to browse
+		the documentation of <a href="%(adapter)s/WebKit/Docs/">WebKit</a>
+		and <a href="%(adapter)s/Docs/ComponentIndex.html">all other components</a>
+		of <a href="%(adapter)s/Docs/">Webware for Python</a>.</p>''' % locals())
 		from os.path import join
 		self.writeln('<p>The location of the WebKit documentation on the server is:</p>')
 		self.writeln('<blockquote>%s</blockquote>' % join(self.application().webKitPath(), 'Docs'))
