896f1c04389c7d43d201477c5e4cb1ef9ff9133c
Christoph Zwerschke <cito@online.de>
2005-10-28 00:19:19 +0000

    Cleaned mod_webkit code so that it will not output dozens of warnings during compilation any more.

--- WebKit/Adapters/mod_webkit/marshal.c
+++ WebKit/Adapters/mod_webkit/marshal.c
@@ -1,181 +1,135 @@
-
-/* marshal.c */
-/* author: Jay Love (jsliv@jslove.org) */
-/* Adapted from marshal.c in the python distribution */
-/* This file handles the creation of python marshal structures. */
-
-
+/**************************************************************
+ * marshal.c                                                  *
+ * Handles the creation of Python marshal structures          *
+ * Adapted from marshal.c in the Python distribution          *
+ * Author: Jay Love (jsliv@jslove.org)                        *
+**************************************************************/
 
 #include <string.h>
 #include "mod_webkit.h"
 
-
-
 /*
-#define w_byte(c, p) if ((p)->ptr != (p)->end) *(p)->ptr++ = (c); \
-			   else w_more(c, p)
+#define w_byte(c, p) if ((p)->ptr != (p)->end) *(p)->ptr++ = (c); else w_more(c, p)
 */
-
-
-WFILE* new_item()
-{
-		WFILE* wf;
-}
-		
-
 
 char* expand_memory(WFILE* p, long add)
 {
-		char* newptr;
-		long currsize;
-		long newsize = 0;
-		char log_msg[500];
-		
-		//log_message("Expanding Memory",p->r);
+    char* newptr;
+    long currsize;
+    long newsize = 0;
+    //char log_msg[500];
 
-		currsize = p->end - p->str;
-		if (add == 0) add = 4096;
+    //log_message("Expanding Memory",p->r);
 
-		newsize = currsize + add;
-		
-		//sprintf(log_msg,"Expanding Memory from %i to %i", currsize, newsize);
-		//log_message(log_msg, p->r);
-		newptr = ap_pcalloc(p->r->pool, newsize);
-		//if !newptr  //need a check here 
+    currsize = p->end - p->str;
+    if (add == 0) add = 4096;
 
-		memcpy( newptr, p->str, currsize);
-		p->end = newptr + newsize;
-		p->ptr = newptr + (p->ptr - p->str);
-		p->str = newptr;
+    newsize = currsize + add;
 
-		//log_message("Memory Expanded", p->r);
-		return newptr;
+    //sprintf(log_msg,"Expanding Memory from %i to %i", currsize, newsize);
+    //log_message(log_msg, p->r);
+    newptr = ap_pcalloc(p->r->pool, newsize);
+    //if (!newptr) -- need a check here
+
+    memcpy(newptr, p->str, currsize);
+    p->end = newptr + newsize;
+    p->ptr = newptr + (p->ptr - p->str);
+    p->str = newptr;
+
+    //log_message("Memory Expanded", p->r);
+    return newptr;
 }
 
-
-
-
-void
-insert_data(WFILE* dest, WFILE* src) {
-
-		long src_len, dest_avail, len_need;
-		char logmsg[500];
-		
-		//log_message("inserting data", dest->r);
-
-		src_len = src->ptr - src->str;
-		dest_avail = dest->end - dest->ptr;
-		len_need = src_len - dest_avail;
-		if (len_need > 0) {  // potential off by one here
-				expand_memory(dest, len_need+2);
-		}
-		memcpy(dest->ptr, src->str, src_len);
-		dest->ptr = dest->ptr + src_len;
-		//log_message("done inserting data", dest->r);
-		
-}
-
-
-
-void
-w_more(c, p)
-	char c;
-	WFILE *p;
+void insert_data(WFILE* dest, WFILE* src)
 {
-	int size, newsize;
-	char* newptr;
+    long src_len, dest_avail, len_need;
+    //char log_msg[500];
 
-//	log_message("In w_more", p->r);
-	if (p->str == NULL)
-		return; /* An error already occurred, we're screwed */
+    //log_message("inserting data", dest->r);
 
-	expand_memory(p, 0);
-	*p->ptr++ = c;
+    src_len = src->ptr - src->str;
+    dest_avail = dest->end - dest->ptr;
+    len_need = src_len - dest_avail;
+    if (len_need > 0) { /* potential off by one here */
+        expand_memory(dest, len_need+2);
+    }
+    memcpy(dest->ptr, src->str, src_len);
+    dest->ptr = dest->ptr + src_len;
+    //log_message("done inserting data", dest->r);
+
 }
 
-
-void w_byte(char c, WFILE* p) {
-if ((p)->ptr != (p)->end) 
-		*(p)->ptr++ = (c);
-else w_more(c, p);
-}
-
-
-
-void
-w_string(s, n, p)
-	char *s;
-	int n;
-	WFILE *p;
+void w_more(int c, WFILE *p)
 {
-//		log_message("In w_string", p->r);
-		while (--n >= 0) {
-			w_byte(*s, p);
-			s++;
-   		}
+    //log_message("In w_more", p->r);
+    if (p->str == NULL)
+        return; /* An error already occurred, we're screwed */
+    expand_memory(p, 0);
+    *p->ptr++ = c;
 }
 
-void
-w_short(x, p)
-	int x;
-	WFILE *p;
+void w_byte(char c, WFILE* p)
 {
-	w_byte( x      & 0xff, p);
-	w_byte((x>> 8) & 0xff, p);
+    if ((p)->ptr != (p)->end)
+        *(p)->ptr++ = (c);
+    else
+        w_more(c, p);
 }
 
-void
-w_long(x, p)
-	long x;
-	WFILE *p;
+void w_string(const char *s, int n, WFILE *p)
 {
-	w_byte((int)( x      & 0xff), p);
-	w_byte((int)((x>> 8) & 0xff), p);
-	w_byte((int)((x>>16) & 0xff), p);
-	w_byte((int)((x>>24) & 0xff), p);
+    //log_message("In w_string", p->r);
+    while (--n >= 0) {
+        w_byte(*s, p);
+        s++;
+    }
+}
+
+void w_short(int x, WFILE *p)
+{
+    w_byte( x      & 0xff, p);
+    w_byte((x>> 8) & 0xff, p);
+}
+
+void w_long(long x, WFILE *p)
+{
+    w_byte((int)( x      & 0xff), p);
+    w_byte((int)((x>> 8) & 0xff), p);
+    w_byte((int)((x>>16) & 0xff), p);
+    w_byte((int)((x>>24) & 0xff), p);
 }
 
 #if SIZEOF_LONG > 4
-void
-w_long64(x, p)
-	long x;
-	WFILE *p;
+void w_long64(long x, WFILE *p)
 {
-	w_long(x, p);
-	w_long(x>>32, p);
+    w_long(x, p);
+    w_long(x>>32, p);
 }
 #endif
 
-
-void
-write_string( char* s, long len, WFILE* p){
-
-		w_byte(TYPE_STRING, p);
-		w_long(len, p);
-		if (len > 0)
-				w_string( s, len, p);
-		//log_message(s,p->r);
+void write_string(const char* s, WFILE* p)
+{
+    int len = (int)strlen(s);
+    w_byte(TYPE_STRING, p);
+    w_long((long)len, p);
+    w_string(s, len, p);
+    //log_message(s,p->r);
 }
 
-void
-write_integer(int number, WFILE* wf) {
-		long x,y;
-		x = (long)number;
+void write_integer(int number, WFILE* wf)
+{
+    long x;
+    x = (long)number;
 #if SIZEOF_LONG > 4
-		long y = x>>31;
-		if (y && y != -1) {
-			w_byte(TYPE_INT64, wf);
-			w_long64(x, wf);
-		}
-		else
+    long y = x>>31;
+    if (y && y != -1) {
+        w_byte(TYPE_INT64, wf);
+        w_long64(x, wf);
+    }
+    else
 #endif
-			{
-			w_byte(TYPE_INT, wf);
-			w_long(x, wf);
-		}
+    {
+        w_byte(TYPE_INT, wf);
+        w_long(x, wf);
+    }
 }
-
-
-
-
-
--- WebKit/Adapters/mod_webkit/mod_webkit.h
+++ WebKit/Adapters/mod_webkit/mod_webkit.h
@@ -8,43 +8,44 @@
 #include "http_request.h"
 #include "util_script.h"
 
+#define HUGE_STRING_LENGTH  4096
 
-
-
-
-
-#define HUGE_STRING_LENGTH  4096 
-#define TYPE_NULL	'0'
-#define TYPE_NONE	'N'
+#define TYPE_NULL       '0'
+#define TYPE_NONE       'N'
 #define TYPE_ELLIPSIS   '.'
-#define TYPE_INT	'i'
-#define TYPE_INT64	'I'
-#define TYPE_FLOAT	'f'
-#define TYPE_COMPLEX	'x'
-#define TYPE_LONG	'l'
-#define TYPE_STRING	's'
-#define TYPE_TUPLE	'('
-#define TYPE_LIST	'['
-#define TYPE_DICT	'{'
-#define TYPE_CODE	'c'
-#define TYPE_UNICODE	'u'
-#define TYPE_UNKNOWN	'?'
+#define TYPE_INT        'i'
+#define TYPE_INT64      'I'
+#define TYPE_FLOAT      'f'
+#define TYPE_COMPLEX    'x'
+#define TYPE_LONG       'l'
+#define TYPE_STRING     's'
+#define TYPE_TUPLE      '('
+#define TYPE_LIST       '['
+#define TYPE_DICT       '{'
+#define TYPE_CODE       'c'
+#define TYPE_UNICODE    'u'
+#define TYPE_UNKNOWN    '?'
 
 typedef struct {
-/*
-/*	FILE *fp;
-/*	int error;
-/*	int depth;
-/*	/* If fp == NULL, the following are valid: */
-/*	PyObject *str;
-*/
-
-	char *str; 
-	char *ptr;
-	char *end;
-	pool  *appool;
-	request_rec* r; //just for debugging
+    char *str;
+    char *ptr;
+    char *end;
+    pool  *appool;
+    request_rec* r; /* just for debugging */
 } WFILE;
 
+char* expand_memory(WFILE* p, long add);
+void insert_data(WFILE* dest, WFILE* src);
+void w_more(int c, WFILE *p);
+void w_byte(char c, WFILE* p);
+void w_string(const char *s, int n, WFILE *p);
+void w_short(int x, WFILE *p);
+void w_long(long x, WFILE *p);
+#if SIZEOF_LONG > 4
+void w_long64(long x, WFILE *p);
+#endif
+void write_string(const char* s, WFILE* p);
+void write_integer(int number, WFILE* wf);
 
 int log_message(char* msg, request_rec* r);
+WFILE* setup_WFILE(request_rec* r);
