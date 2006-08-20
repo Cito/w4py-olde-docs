374b38c9ebd80b6a8c8b3cba67465975c36131fe
Christoph Zwerschke <cito@online.de>
2006-08-20 18:05:12 +0000

    Created new mod_webkit.dll libraries for Apache 1.3 and Apache 2.2 on Windows.

--- WebKit/Adapters/mod_webkit2/msvctk.bat
+++ WebKit/Adapters/mod_webkit2/msvctk.bat
@@ -11,12 +11,12 @@ Set PATH=%ProgramFiles%\Microsoft Platform SDK\bin;%PATH%
 Set INCLUDE=%ProgramFiles%\Microsoft Platform SDK\include;%INCLUDE%
 Set LIB=%ProgramFiles%\Microsoft Platform SDK\lib;%LIB%
 
-Set APACHE=%ProgramFiles%\Apache Group\Apache2
+Set APACHE=%ProgramFiles%\Apache Software Foundation\Apache2.2
 
 Set PATH=%Apache%\bin;%PATH%
 Set INCLUDE=%Apache%\include;%INCLUDE%
 Set LIB=%Apache%\lib;%LIB%
 
-cl /MT /W3 /GX /O2 /YX /FD /LD /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "MOD_WEBKIT_EXPORTS" /I "%Apache%\include" mod_webkit.c marshal.c /link libapr.lib libaprutil.lib libhttpd.lib kernel32.lib ws2_32.lib
+cl /MT /W3 /GX /O2 /YX /FD /LD /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "MOD_WEBKIT_EXPORTS" /I "%Apache%\include" mod_webkit.c marshal.c /link libapr-1.lib libaprutil-1.lib libhttpd.lib kernel32.lib ws2_32.lib
 
 copy mod_webkit.dll "%Apache%\modules\mod_webkit.so"
