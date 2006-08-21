db6db9b33cc7b358c4678fd72bd114c92797da45
Christoph Zwerschke <cito@online.de>
2006-08-21 01:52:44 +0000

    Made Test Launch script backward compatible.

--- WebKit/Tests/Basic/Launch.py
+++ WebKit/Tests/Basic/Launch.py
@@ -2,7 +2,11 @@
 
 import os, sys
 
-os.chdir(os.path.abspath(os.path.dirname(__file__)))
+scriptName = sys.argv and sys.argv[0]
+if not scriptName or scriptName == '-c':
+	scriptName = 'Launch.py'
+workDir = os.path.dirname(os.path.abspath(scriptName))
+os.chdir(workDir)
 webwareDir = os.path.join(os.pardir, os.pardir, os.pardir)
 sys.path.insert(0, webwareDir)
 
