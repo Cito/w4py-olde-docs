d89c708b27d2233cd911ea547a110644eaea3983
Christoph Zwerschke <cito@online.de>
2007-11-24 04:57:02 +0000

    Added some more editor commands to editfile.py.

--- bin/editfile.py
+++ bin/editfile.py
@@ -2,18 +2,36 @@
 
 # Helper script for the feature provided by the IncludeEditLink setting.
 
-editor = 'gnuclient (Emacs)'
+editor = 'Vim'
 
 editorCommands = {
-	'gnuclient (Emacs)':
-		'gnuclient +%(line)s %(filename)s',
+	'Emacs':
+		'gnuclient +%(line)s "%(filename)s"',
+	'Geany':
+		'geany -l %(line)s "%(filename)s"',
 	'Geany (Windows)':
 		r'start %%ProgramFiles%%\Geany\Geany.exe -l %(line)s "%(filename)s"',
+	'gedit':
+		'gedit +%(line)s "%(filename)s"',
+	'jEdit':
+		'jedit "%(filename)s" +line:%(line)s',
+	'Kate':
+		'kate -u -l %(line)s "%(filename)s"',
+	'KWrite':
+		'kwrite --line %(line)s "%(filename)s"',
+	'Pico':
+		'pico +%(line)s "%(filename)s"',
 	'PSPad (Windows)':
 		r'start %%ProgramFiles%%\PSPad\PSPad.exe -%(line)s "%(filename)s"',
+	'SciTE':
+		'scite "%(filename)s" -goto:%(line)s',
 	'SciTE (Windows)':
 		r'start %%ProgramFiles%%\SciTE\SciTE.exe "%(filename)s" -goto:%(line)s',
+	'Vim':
+		'gvim +%(line)s "%(filename)s"',
 	}
+
+defaultCommand = editor + ' +%(line)s "%(filename)s"'
 
 from os import system
 from sys import argv
@@ -23,7 +41,7 @@ except ImportError:
 	from rfc822 import Message as message_from_file
 
 def openFile(params):
-	command = editorCommands[editor] % params
+	command = editorCommands.get(editor, defaultCommand) % params
 	system(command)
 
 def parseFile(filename):
