a69accf8f3a4c118bd5c8484c2005f4ed75d42ba
Christoph Zwerschke <cito@online.de>
2007-12-20 17:02:05 +0000

    UserManagerTest did not work when database was password protected.

--- UserKit/Tests/UserManagerTest.py
+++ UserKit/Tests/UserManagerTest.py
@@ -219,8 +219,9 @@ class UserManagerToMiddleKitTest(_UserManagerToSomewhereTest):
 			self._mysqlClient = self._mysqlTestInfo['mysqlClient']
 			mysqlClientName = os.path.basename(self._mysqlClient)
 			assert mysqlClientName == 'mysql' or mysqlClientName == 'mysql.exe'
-			self._mysqlClient = ' '.join([self._mysqlClient] + ['--%s="%s"' % s
-					for s in self._mysqlTestInfo['DatabaseArgs'].items() if s[1]])
+			self._mysqlClient = ' '.join([self._mysqlClient] + ['--%s="%s"'
+				% (p.replace('passwd', 'password'), v) for (p ,v)
+					in self._mysqlTestInfo['DatabaseArgs'].items() if v])
 			executeSqlCmd = '%s < %s' % (self._mysqlClient, create_sql)
 
 			# _log.debug('running: %s', executeSqlCmd)
