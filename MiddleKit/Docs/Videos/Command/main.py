a7246a2e67b9bf3cf9d5ed3ea48bba5f43e512fe
Christoph Zwerschke <cito@online.de>
2008-11-05 20:51:45 +0000

    Use datetime instead of mx.DateTime in Quick Start example.

--- MiddleKit/Docs/Videos/Command/main.py
+++ MiddleKit/Docs/Videos/Command/main.py
@@ -2,7 +2,7 @@
 import os, sys
 sys.path.insert(1, os.path.abspath(os.pardir))
 
-from mx.DateTime import DateFrom
+from datetime import date
 from MiddleKit.Run.MySQLObjectStore import MySQLObjectStore
 from Middle.Movie import Movie
 from Middle.Person import Person
@@ -24,12 +24,12 @@ def main():
 
 	james = Person()
 	james.setName('James Cameron')
-	james.setBirthDate(DateFrom('8/16/1954'))
+	james.setBirthDate(date(1954, 8, 16))
 	movie.addToDirectors(james)
 
 	ahnuld = Person()
 	ahnuld.setName('Arnold Schwarzenegger')
-	ahnuld.setBirthDate(DateFrom('7/30/1947'))
+	ahnuld.setBirthDate(date(1947, 7, 30))
 	store.addObject(ahnuld)
 
 	terminator = Role()
