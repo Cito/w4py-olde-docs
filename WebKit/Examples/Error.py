e59d09cb4d8213631c8550350ca48993d9757124
Christoph Zwerschke <cito@online.de>
2009-01-25 13:41:36 +0000

    Improved the error example servlet. The error is now only raised after you click a button, and you can choose the type of the error (including old style string exception, to ensure these work as well).

--- WebKit/Examples/Error.py
+++ WebKit/Examples/Error.py
@@ -3,6 +3,29 @@ from ExamplePage import ExamplePage
 
 class Error(ExamplePage):
 
-	def writeBody(self):
-		self.write('<p>About to raise an exception...</p>')
-		import UnknownModule
+	def title(self):
+		return 'Error raising Example'
+
+	def writeContent(self):
+		error = self.request().field('error', None)
+		if error:
+			msg = 'You clicked that button!'
+			if error.startswith('String'):
+				error = msg
+			elif error.startswith('System'):
+				error = SystemError(msg)
+			else:
+				error = StandardError(msg)
+			self.writeln('<p>About to raise an error...</p>')
+			raise error
+		self.writeln("""
+<h1>Error Test</h1>
+<form action="Error">
+<p><select name="error" size="1">
+<option selected>StandardError</option>
+<option>SystemError</option>
+<option>String error (old)</option>
+</select>
+<input type="submit" value="Don't click this button!"></p>
+</form>
+""")
