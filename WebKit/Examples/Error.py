efedb08397ac8c1eb55d1c26571a70befa9700a1
Christoph Zwerschke <cito@online.de>
2009-01-31 22:53:44 +0000

    Improved Error example servlet and its twill test so that it works with Python 2.6.

--- WebKit/Examples/Error.py
+++ WebKit/Examples/Error.py
@@ -1,5 +1,14 @@
 from ExamplePage import ExamplePage
 
+class CustomError:
+	"""Custom classic class not based on Exception (for testing)."""
+
+	def __init__(self, msg):
+		self.msg = msg
+
+	def __str__(self):
+		return self.msg
+
 
 class Error(ExamplePage):
 
@@ -12,6 +21,8 @@ class Error(ExamplePage):
 			msg = 'You clicked that button!'
 			if error.startswith('String'):
 				error = msg
+			elif error.startswith('Custom'):
+				error = CustomError(msg)
 			elif error.startswith('System'):
 				error = SystemError(msg)
 			else:
@@ -21,9 +32,10 @@ class Error(ExamplePage):
 		self.writeln('''<h1>Error Test</h1>
 <form action="Error" method="post">
 <p><select name="error" size="1">
-<option selected>StandardError</option>
-<option>SystemError</option>
-<option>String error (old)</option>
+<option selected>Standard Error</option>
+<option>System Error</option>
+<option>Custom Class (old)</option>
+<option>String (deprecated)</option>
 </select>
 <input type="submit" value="Don't click this button!"></p>
 </form>''')
