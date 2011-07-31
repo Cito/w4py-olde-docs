a93acf3a8c7172b6295711fd25e42a2da176e27c
Christoph Zwerschke <cito@online.de>
2011-07-31 12:06:18 +0000

    Adapted make file for Windows to run with VS 2010.

--- WebKit/Adapters/wkcgi/make.bat
+++ WebKit/Adapters/wkcgi/make.bat
@@ -1,18 +1,16 @@
 @echo off
 
 rem Batch file for generating the wkcgi.exe CGI executable
-rem using the free Microsoft Visual C++ 2008 Express Edition
+rem using the free Microsoft Visual C++ 2010 Express Edition
 rem (download at http://www.microsoft.com/express/download/).
 
-rem Set environment variables
-
-set VC=%ProgramFiles%\Microsoft Visual Studio 9.0\VC
-set APACHE=%ProgramFiles%\Apache Software Foundation\Apache2.2
+rem Path to Visual Studio (Visual C++) 10
+rem (32bit version may be under %ProgramFiles(x86)% on 64bit systems)
+set VC=%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC
 
 call "%VC%\vcvarsall"
 
 rem Compile and link wkcgi
-
 cl /W3 /O2 /EHsc /wd4996 ^
     /D WIN32 /D _CONSOLE /D _MBCS  ^
     wkcgi.c ^
@@ -22,10 +20,8 @@ cl /W3 /O2 /EHsc /wd4996 ^
 
 
 rem Remove all intermediate results
-
 del /Q *.exp *.ilk *.lib *.obj *.pdb
 
 rem Wait for keypress before leaving
-
 echo.
 pause
